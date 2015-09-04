67c3fd6ce2fb7cb69d35f77877df0dc541748e08
Eric Pilger <pilger@hsfl.hawaii.edu>
2015-09-03 16:09:04 -1000

    Merged Miguels' Udp class with socketlib.

--- libraries/support/socketlib.h
+++ libraries/support/socketlib.h
@@ -145,6 +145,91 @@ int32_t socket_blocking(socket_channel *channel, bool blocking);
 int32_t socket_close(socket_channel *channel);
 vector<socket_channel> socket_find_addresses(uint16_t ntype);
 
+//-------------------------------------------------------------------
+// Simple UDP class to send data
+
+struct SocketOptions
+{
+	// set the defaults
+	// Channel type
+	int32_t type = SOCKET_TYPE_UDP;
+	// UDP socket handle
+	int32_t handle = 0;
+	// UDP INET4 address
+	struct sockaddr_in server;
+	struct sockaddr_in s_other;
+	// to use socket connect function
+	bool connect = false;
+
+	// Channel's protocol address in string form
+	string address = "127.0.0.1";
+	// port
+	uint16_t port = 8888;
+
+	// Length for chosen address
+	int addrlen = sizeof(server);
+
+	uint16_t role = SOCKET_TALK;
+	bool blocking = SOCKET_BLOCKING;
+	uint32_t timeout = SOCKET_RCVTIMEO;
+
+	bool stream = false; // for SOCK_STREAM in simgen
+
+public:
+	// constructor
+	SocketOptions(){}
+	//SocketOptions():type(0),handle(0),address(""),port(),addrlen(0),role(0),blocking(false),timeout(0){}
+	SocketOptions(uint16_t p):port{p}{}
+	SocketOptions(string a, uint16_t p):address{a},port{p}{}
+	SocketOptions(string a, uint16_t p, uint16_t r):address{a},port{p},role{r}{}
+
+} ;
+
+class Udp
+{
+
+private:
+	// agent stuff
+	//socket_channel socket;
+	SocketOptions sok;
+	//beatstruc hbeat;
+
+	int32_t iretn = 0; // return error
+
+	//int32_t openClient();
+	int32_t openServer();
+	int32_t errorStatus(string functionName);
+
+public:
+	// constructors
+	Udp(); // : sok("127.0.0.1",8888){}
+	//Udp(uint16_t p);
+	//Udp(string a, uint16_t p);
+	//Udp(string a, uint16_t p, uint16_t r);
+
+	int32_t socketOpen();
+
+	int32_t setupClient();
+	int32_t setupClient(string a, uint16_t p);
+
+	int32_t setupClientSimGen(string a, uint16_t p);
+	int32_t setupClientAcstb(string a, uint16_t p);
+
+	//int32_t setupServer();
+	//int32_t setupServer(uint16_t p);
+	int32_t setupServer(uint16_t port, float timeout_sec);
+
+
+	int32_t send(string package2send);
+	int32_t receiveLoop();
+	int32_t receiveOnce();
+
+	int32_t close();
+
+	string receivedData; // container for received data
+
+};
+
 //! @}
 
 #endif // SOCKETLIB_H
