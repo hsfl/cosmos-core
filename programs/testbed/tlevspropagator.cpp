4992ef828c0d254ef469cf928ce2d307a775dbc3
Eric Pilger <pilger@hsfl.hawaii.edu>
2015-09-25 10:06:30 -1000

    tweaks

--- programs/testbed/tlevspropagator.cpp
+++ programs/testbed/tlevspropagator.cpp
@@ -183,9 +183,9 @@ int main(int argc, char *argv[])
 		}
 		kepstruc kep;
 		eci2kep(eci, kep);
-		printf("%.15g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t", cmjd, eci.s.col[0], eci.s.col[1], eci.s.col[2], cdata->node.loc.pos.geod.s.col[0], cdata->node.loc.pos.geod.s.col[1], cdata->node.loc.pos.geod.s.col[2]);
-		printf("%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t", eci.v.col[0], eci.v.col[1], eci.v.col[2], cdata->node.loc.pos.geod.v.col[0], cdata->node.loc.pos.geod.v.col[1], cdata->node.loc.pos.geod.v.col[2]);
-		printf("%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t", eci.a.col[0], eci.a.col[1], eci.a.col[2], cdata->node.loc.pos.geod.a.col[0], cdata->node.loc.pos.geod.a.col[1], cdata->node.loc.pos.geod.a.col[2]);
+		printf("%.15g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t", cmjd, eci.s.col[0], eci.s.col[1], eci.s.col[2], cdata->node.loc.pos.geod.s.h, cdata->node.loc.pos.geod.s.lat, cdata->node.loc.pos.geod.s.lon);
+		printf("%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t", eci.v.col[0], eci.v.col[1], eci.v.col[2], cdata->node.loc.pos.geod.v.h, cdata->node.loc.pos.geod.v.lat, cdata->node.loc.pos.geod.v.lon);
+		printf("%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t", eci.a.col[0], eci.a.col[1], eci.a.col[2], cdata->node.loc.pos.geod.a.h, cdata->node.loc.pos.geod.a.lat, cdata->node.loc.pos.geod.a.lon);
 		printf("%.10g\t%.10g\t%.10g\t%.10g\t%.10g\t%.10g\n", kep.ea, kep.e, kep.a, kep.raan, kep.i, kep.ap);
 	}
 }
