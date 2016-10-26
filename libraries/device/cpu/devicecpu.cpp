26e9ba365da3a23132bbb467ab8338f7120fee74
Eric Pilger <pilger@hsfl.hawaii.edu>
2016-10-26 10:57:35 -1000

    Fixes to Jim's changes to allow compilation in Windows.

--- libraries/device/cpu/devicecpu.cpp
+++ libraries/device/cpu/devicecpu.cpp
@@ -54,7 +54,7 @@ double DeviceCpu::getMemoryTotal(){
 #endif
 
 #if defined(COSMOS_WIN_OS)
-    memoryTotal = cpuWin.getMemoryTotal();
+    memoryTotal = cpuWin.getVirtualMemoryTotal();
 #endif
     return memoryTotal;
 }
@@ -67,7 +67,7 @@ double DeviceCpu::getMemoryUsed(){
 #endif
 
 #if defined(COSMOS_WIN_OS)
-    memoryUsed = cpuWin.getMemoryUsed();
+    memoryUsed = cpuWin.getVirtualMemoryUsed();
 #endif
     return memoryUsed;
 }
@@ -798,8 +798,7 @@ unsigned long long DeviceCpuWindows::FileTimeToInt64(const FILETIME & ft)
     return (((unsigned long long)(ft.dwHighDateTime))<<32)|((unsigned long long)ft.dwLowDateTime);
 }
 
-#endif
-
+#else
 DeviceCpuLinux::procPidStat::procPidStat(string processName)
 {
     DeviceCpu dev;
@@ -860,3 +859,4 @@ DeviceCpuLinux::procStat::procStat()
     time_total = stoi(user) + stoi(nice) + stoi(system) + stoi(idle) + stoi(iowait) + stoi(irq) + stoi(softirq) + stoi(steal) + stoi(guest) + stoi(guest_nice);
 
 }
+#endif
