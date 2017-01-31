617fb4e87b993bce48050aa9576ae626117a6c46
Eric Pilger <pilger@hsfl.hawaii.edu>
2017-01-31 12:01:21 -1000

    Changed to use universal ${COSMOS}

--- cmake/use_cosmos_from_source.cmake
+++ cmake/use_cosmos_from_source.cmake
@@ -1,41 +1,41 @@
 if(${USE_COSMOS_FROM} MATCHES "SOURCE")
-    message("using COSMOS from source folder " ${COSMOS_SOURCE})
+    message("using COSMOS from source folder " ${COSMOS}/src)
 
     # -----------------------------------------------
     # add the cosmos libraries
-    include_directories(${COSMOS_SOURCE}/core/libraries/)
-    include_directories(${COSMOS_SOURCE}/core/libraries/support)
+    include_directories(${COSMOS}/src/core/libraries/)
+    include_directories(${COSMOS}/src/core/libraries/support)
 
-    include_directories(${COSMOS_SOURCE}/core/libraries/thirdparty)
+    include_directories(${COSMOS}/src/core/libraries/thirdparty)
 
-    add_subdirectory(${COSMOS_SOURCE}/core/libraries/agent     ${CMAKE_BINARY_DIR}/libraries/agent/)
-    add_subdirectory(${COSMOS_SOURCE}/core/libraries/support   ${CMAKE_BINARY_DIR}/libraries/support/)
-    add_subdirectory(${COSMOS_SOURCE}/core/libraries/math      ${CMAKE_BINARY_DIR}/libraries/math/)
-    add_subdirectory(${COSMOS_SOURCE}/core/libraries/physics   ${CMAKE_BINARY_DIR}/libraries/physics/)
+    add_subdirectory(${COSMOS}/src/core/libraries/agent     ${CMAKE_BINARY_DIR}/libraries/agent/)
+    add_subdirectory(${COSMOS}/src/core/libraries/support   ${CMAKE_BINARY_DIR}/libraries/support/)
+    add_subdirectory(${COSMOS}/src/core/libraries/math      ${CMAKE_BINARY_DIR}/libraries/math/)
+    add_subdirectory(${COSMOS}/src/core/libraries/physics   ${CMAKE_BINARY_DIR}/libraries/physics/)
 
 
     # -----------------------------------------------
-    add_subdirectory(${COSMOS_SOURCE}/core/libraries/thirdparty/zlib     ${CMAKE_BINARY_DIR}/libraries/zlib/)
+    add_subdirectory(${COSMOS}/src/core/libraries/thirdparty/zlib     ${CMAKE_BINARY_DIR}/libraries/zlib/)
     if(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC") #or just MSVC
-        add_subdirectory(${COSMOS_SOURCE}/core/libraries/thirdparty/dirent     ${CMAKE_BINARY_DIR}/libraries/dirent/)
+        add_subdirectory(${COSMOS}/src/core/libraries/thirdparty/dirent     ${CMAKE_BINARY_DIR}/libraries/dirent/)
     endif()
 
     # -----------------------------------------------
     # import devices
-    include_directories(${COSMOS_SOURCE}/core/libraries/device)
-    include_directories(${COSMOS_SOURCE}/core/libraries/device/general)
+    include_directories(${COSMOS}/src/core/libraries/device)
+    include_directories(${COSMOS}/src/core/libraries/device/general)
 
-    add_subdirectory(${COSMOS_SOURCE}/core/libraries/device/general      ${CMAKE_BINARY_DIR}/libraries/device/general)
+    add_subdirectory(${COSMOS}/src/core/libraries/device/general      ${CMAKE_BINARY_DIR}/libraries/device/general)
 
     # -----------------------------------------------
     # import device disk
-    include_directories(${COSMOS_SOURCE}/core/libraries/device/disk)
-    add_subdirectory(   ${COSMOS_SOURCE}/core/libraries/device/disk      ${CMAKE_BINARY_DIR}/libraries/device/disk)
+    include_directories(${COSMOS}/src/core/libraries/device/disk)
+    add_subdirectory(   ${COSMOS}/src/core/libraries/device/disk      ${CMAKE_BINARY_DIR}/libraries/device/disk)
 
     # -----------------------------------------------
     # import device cpu
-    include_directories(${COSMOS_SOURCE}/core/libraries/device/cpu)
-    add_subdirectory(   ${COSMOS_SOURCE}/core/libraries/device/cpu      ${CMAKE_BINARY_DIR}/libraries/device/cpu)
+    include_directories(${COSMOS}/src/core/libraries/device/cpu)
+    add_subdirectory(   ${COSMOS}/src/core/libraries/device/cpu      ${CMAKE_BINARY_DIR}/libraries/device/cpu)
 
     #add_library(myzlib STATIC IMPORTED)
     #set_property(TARGET myzlib PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/libraries/thirdparty/zlib/libzlib.a)
