322922831e2ae8ad8becf742880c42c6b5c7fe59
Eric Pilger <pilger@hsfl.hawaii.edu>
2017-02-15 19:23:50 -1000

    Modified to account for overhead due to things like assignment.

--- programs/tests/mathspeed.cpp
+++ programs/tests/mathspeed.cpp
@@ -9,10 +9,13 @@
 
 #define BUFSIZE 10000000
 
-double buffer[10001][1000];
+double buffer1[10001];
+double buffer2[10001];
+double buffer3a[100][100];
+double buffer3b[1000][1000];
+double buffer3c[10000][10000];
 char *srcbuf;
 ElapsedTime et;
-//struct timeval tp;
 unsigned long size, count;
 long start_s, start_u, diff_u, time1, time2, time3, time4, time5;
 double start_t;
@@ -20,123 +23,143 @@ long i, j, k, ii;
 int inb;
 struct stat sbuf;
 double answer, base, speed1, speed2, speed3, speed4, speed5;
+size_t loopcnt;
 
 main(int argc, char **argv)
 {
     speed1 = speed2 = speed3 = 0.;
     time1 = time2 = time3 =time4 = time5 = 1000000000L;
 
-    for (i=0;i<10001;i++)
-        for (j=0; j<1000; j++)
-            buffer[i][j] = j%256/100.;
-
-    for (k=0; k<5; k++)
+    for (size_t i=0; i<10000; ++i)
     {
-        //gettimeofday(&tp,NULL);
-        //start_s = tp.tv_sec;
-        //start_u = tp.tv_usec;
-        et.reset();
-        j = 5000000;
-        for (i=0;i<5000;i++)
-        {
-            for (j=0; j<1000; j++)
-            {
-                answer = (double)25.;
-            }
-        }
-        //gettimeofday(&tp,NULL);
-        //diff_u = 1000000L*(tp.tv_sec-start_s)+(tp.tv_usec-start_u);
-        diff_u = 1000000L * et.lap();
-        if (time1 > diff_u)
-            time1 = diff_u;
-        base = 5000000./diff_u;
-        if (speed1 < base)
-            speed1 = base;
-//        fflush(stdout);
-        //gettimeofday(&tp,NULL);
-        //start_s = tp.tv_sec;
-        //start_u = tp.tv_usec;
-        et.reset();
-        for (i=0; i<5000; i++)
-        {
-            for (j=0; j<1000; j++)
-            {
-                answer = (double)(5.) * (double)(5.);
-            }
-        }
-        //gettimeofday(&tp,NULL);
-        //diff_u = 1000000L*(tp.tv_sec-start_s)+(tp.tv_usec-start_u);
-        diff_u = 1000000L * et.lap();
-        if (time2 > diff_u)
-            time2 = diff_u;
-        base = 5000000./(time2);
-        if (speed2 < base)
-            speed2 = base;
-//        fflush(stdout);
-        //gettimeofday(&tp,NULL);
-        //start_s = tp.tv_sec;
-        //start_u = tp.tv_usec;
-        et.reset();
-        for (i=0; i<200; i++)
-        {
-            for (ii=0;ii<25;ii++)
-            {
-                for (j=0; j<1000; j++)
-                {
-                    answer = buffer[0][j] * buffer[0][ii];
-                }
-            }
-        }
-        //gettimeofday(&tp,NULL);
-        //diff_u = 1000000L*(tp.tv_sec-start_s)+(tp.tv_usec-start_u);
-        diff_u = 1000000L * et.lap();
-        if (time3 > diff_u)
-            time3 = diff_u;
-        base = 5000000./(time3);
-        if (speed3 < base)
-            speed3 = base;
-        //fflush(stdout);
-        //gettimeofday(&tp,NULL);
-        //start_s = tp.tv_sec;
-        //start_u = tp.tv_usec;
-        et.reset();
-        for (i=0; i<5000; i++)
-        {
-            for (j=0; j<1000; j++)
-            {
-                answer = buffer[i][j] * buffer[i+5000][j];
-            }
-        }
-        //gettimeofday(&tp,NULL);
-        //diff_u = 1000000L*(tp.tv_sec-start_s)+(tp.tv_usec-start_u);
-        diff_u = 1000000L * et.lap();
-        if (time4 > diff_u)
-            time4 = diff_u;
-        base = 5000000./(time4);
-        if (speed4 < base)
-            speed4 = base;
-//        fflush(stdout);
-        et.reset();
-        for (i=0; i<5000; i++)
-        {
-            for (j=0; j<1000; j++)
-            {
-                answer = cos(buffer[i][j] * buffer[i+5000][j]);
-            }
-        }
-        //gettimeofday(&tp,NULL);
-        //diff_u = 1000000L*(tp.tv_sec-start_s)+(tp.tv_usec-start_u);
-        diff_u = 1000000L * et.lap();
-        if (time5 > diff_u)
-            time5 = diff_u;
-        base = 5000000./(time5);
-        if (speed5 < base)
-            speed5 = base;
+        buffer1[i] = et.split();
+        buffer2[i] = et.split();
     }
-    printf("%6.3lf MNops ",speed1);
-    printf("%6.3lf Mflops (%.1lf) ",speed2,speed1/speed2);
-    printf("%6.3lf Mflops (%.1lf) ",speed3,speed1/speed3);
-    printf("%6.3lf Mflops (%.1lf)",speed4,speed1/speed4);
-    printf("%6.3lf Mflops (%.1lf)",speed5,speed1/speed5);
+
+    // Check speed of assignment
+    loopcnt = 0;
+    et.reset();
+    do
+    {
+        for (size_t i=0; i<10000; ++i)
+        {
+            buffer1[i] = buffer2[i];
+        }
+        ++loopcnt;
+    } while (et.split() < 3.);
+    double dassign = et.split() / (loopcnt*10000.);
+    printf("%6.3lf Massignps (%.1lf) : ",1e-6/dassign, dassign/dassign);
+    fflush(stdout);
+
+//    // Check speed of split
+//    loopcnt = 0;
+//    et.reset();
+//    do
+//    {
+//        for (size_t i=0; i<10000; ++i)
+//        {
+//            buffer1[i] = et.split();
+//        }
+//        ++loopcnt;
+//    } while (et.split() < 3.);
+//    double dsplit = (et.split() / (loopcnt*10000.)) - dassign;
+//    printf("%6.3lf Msplitps (%.1lf) : ",1e-6/dsplit, dsplit/dassign);
+//    fflush(stdout);
+
+    // Check speed of multiply by constant
+    loopcnt = 0;
+    et.reset();
+    do
+    {
+        for (size_t i=0; i<10000; ++i)
+        {
+            buffer1[i] = 1.234 * buffer2[i];
+        }
+        ++loopcnt;
+    } while (et.split() < 3.);
+    double dscalar = (et.split() / (loopcnt*10000.)) - (dassign);
+//    double dscalar = (et.split() / (loopcnt*10000.));
+    printf("%6.3lf Msflops (%.1lf) : ",1e-6/dscalar, dscalar/dassign);
+    fflush(stdout);
+
+    // Check speed of multiply by variable
+    loopcnt = 0;
+    et.reset();
+    do
+    {
+        for (size_t i=0; i<10000; ++i)
+        {
+            buffer1[i] = buffer1[i] * buffer2[i];
+        }
+        ++loopcnt;
+    } while (et.split() < 3.);
+    double dvariable = (et.split() / (loopcnt*10000.)) - (dassign);
+    printf("%6.3lf Mvflops (%.1lf) : ",1e-6/dvariable, dvariable/dassign);
+    fflush(stdout);
+
+    // Check speed of multiply by variable, 100x100
+    loopcnt = 0;
+    et.reset();
+    do
+    {
+        for (size_t i=0; i<100; ++i)
+        {
+            for (size_t j=0; j<100; ++j)
+            {
+                buffer3a[i][j] = buffer1[i] * buffer2[j];
+            }
+        }
+        ++loopcnt;
+    } while (et.split() < 3.);
+    double dvariable100 = (et.split() / (loopcnt*10000.)) - (dassign);
+    printf("%6.3lf Mv100flops (%.1lf) : ",1e-6/dvariable100, dvariable100/dassign);
+    fflush(stdout);
+
+
+    // Check speed of multiply by variable, 1000x1000
+    loopcnt = 0;
+    for (size_t i=0; i<10000; ++i)
+    {
+        buffer1[i] = et.split();
+        buffer2[i] = et.split();
+    }
+    et.reset();
+    do
+    {
+        for (size_t i=0; i<1000; ++i)
+        {
+            for (size_t j=0; j<1000; ++j)
+            {
+                buffer3a[i][j] = buffer1[i] * buffer2[j];
+            }
+        }
+        ++loopcnt;
+    } while (et.split() < 3.);
+    double dvariable1000 = (et.split() / (loopcnt*1000000.)) - (dassign);
+    printf("%6.3lf Mv1000flops (%.1lf) : ",1e-6/dvariable1000, dvariable1000/dassign);
+    fflush(stdout);
+
+    // Check speed of multiply by variable, 10000x10000
+    loopcnt = 0;
+    for (size_t i=0; i<10000; ++i)
+    {
+        buffer1[i] = et.split();
+        buffer2[i] = et.split();
+    }
+    et.reset();
+    do
+    {
+        for (size_t i=0; i<10000; ++i)
+        {
+            for (size_t j=0; j<10000; ++j)
+            {
+                buffer3a[i][j] = buffer1[i] * buffer2[j];
+            }
+        }
+        ++loopcnt;
+    } while (et.split() < 3.);
+    double dvariable10000 = (et.split() / (loopcnt*100000000.)) - (dassign);
+    printf("%6.3lf Mv10000flops (%.1lf)",1e-6/dvariable10000, dvariable10000/dassign);
     printf("\n");
+
 }
