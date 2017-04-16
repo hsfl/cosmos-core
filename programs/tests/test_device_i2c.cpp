e4ed7861748ca3a68506756f6cfb5f4bec50de8b
Eric Pilger <pilger@hsfl.hawaii.edu>
2017-04-15 16:45:26 -1000

    Adjusted for updated i2c class.

--- programs/tests/test_device_i2c.cpp
+++ programs/tests/test_device_i2c.cpp
@@ -7,8 +7,8 @@ int main(int argc, const char* argv[])
     cout << "testing I2C library" << endl;
     // 0x08 - motor controller board
     // 0x57 - CubeADCS
-    I2C i2c("/dev/i2c-2", 0x08);
-    i2c.send("dsdsa");
+    I2C *i2c = new I2C("/dev/i2c-2", 0x08);
+    i2c->send((uint8_t *)"dsdsa", 5);
 
 }
 
