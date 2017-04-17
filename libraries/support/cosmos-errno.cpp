d3874cb78bc958d0919decdd8f2bd6d8bdf6ccfc
Eric Pilger <pilger@hsfl.hawaii.edu>
2017-04-16 21:57:20 -1000

    Check for invalid response from command.

--- libraries/support/cosmos-errno.cpp
+++ libraries/support/cosmos-errno.cpp
@@ -517,6 +517,9 @@ string cosmos_error_string(int32_t cosmos_errno)
         case CUBEADCS_SERIALCOMM_RESPONSE:
             error_string = "CUBEADCS SERIALCOMM: ADCS REPLY DID NOT MATCH EXPECTED REPLY";
             break;
+        case CUBEADCS_INVALID_RESPONSE:
+            error_string = "Invalid Protocol Response";
+            break;
         default:
             char tval[30];
             sprintf(tval,"Undefined Value %d", cosmos_errno);
