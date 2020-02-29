580573031edd3839f24f0016c92bcc34612197ce
Eric Pilger <ejpilger@gmail.com>
2020-02-29 13:59:50 -1000

    Reorder things so abs can work.

--- programs/tests/track_sband.cpp
+++ programs/tests/track_sband.cpp
@@ -140,6 +140,75 @@ int main(int argc, char *argv[])
 
     // Initialization stuff
 
+    if (nodename.empty())
+    {
+        agent = new Agent("");
+    }
+    else
+    {
+        agent = new Agent(nodename);
+    }
+
+//    if ((iretn = agent->wait()) < 0)
+//    {
+//        fprintf(agent->get_debug_fd(), "%16.10f %s Failed to start Agent %s on Node %s Dated %s : %s\n",currentmjd(), mjd2iso8601(currentmjd()).c_str(), agent->getAgent().c_str(), agent->getNode().c_str(), utc2iso8601(data_ctime(argv[0])).c_str(), cosmos_error_string(iretn).c_str());
+//        exit(iretn);
+//    }
+//    else
+//    {
+//        fprintf(agent->get_debug_fd(), "%16.10f %s Started Agent %s on Node %s Dated %s\n",currentmjd(), mjd2iso8601(currentmjd()).c_str(), agent->getAgent().c_str(), agent->getNode().c_str(), utc2iso8601(data_ctime(argv[0])).c_str());
+//    }
+
+    iretn = json_createpiece(agent->cinfo, antbase.c_str(), DeviceType::ANT);
+    if (iretn < 0)
+    {
+        fprintf(agent->get_debug_fd(), "Failed to add %s ANT %s\n", antbase.c_str(), cosmos_error_string(iretn).c_str());
+        agent->shutdown();
+        exit(iretn);
+    }
+    devindex = agent->cinfo->pieces[static_cast <uint16_t>(iretn)].cidx;
+    antindex = agent->cinfo->device[devindex].ant.didx;
+    agent->cinfo->device[devindex].ant.minelev = RADOF(10.);
+    if (antbase == "sband")
+    {
+        agent->cinfo->device[devindex].ant.model = DEVICE_MODEL_PRKX2SU;
+    }
+    else if (antbase == "yagi")
+    {
+        agent->cinfo->device[devindex].ant.model = DEVICE_MODEL_GS232B;
+    }
+    else
+    {
+        agent->cinfo->device[devindex].ant.model = DEVICE_MODEL_LOOPBACK;
+    }
+
+    iretn = json_dump_node(agent->cinfo);
+    if (iretn < 0)
+    {
+        fprintf(agent->get_debug_fd(), "Failed to save node %s\n", cosmos_error_string(iretn).c_str());
+        exit(iretn);
+    }
+
+    antdevice = "/dev/tty_" + antbase;
+
+    // Connect to antenna and set sensitivity;
+    iretn = connect_antenna();
+    switch (agent->cinfo->device[devindex].all.model)
+    {
+    case DEVICE_MODEL_GS232B:
+        gs232b_set_sensitivity(RADOF(1.));
+        break;
+    case DEVICE_MODEL_PRKX2SU:
+        prkx2su_set_sensitivity(RADOF(.5));
+        break;
+    }
+
+    agent->cinfo->node.loc.pos.geod.s.lat = RADOF(21.969188888888888888888888888889);
+    agent->cinfo->node.loc.pos.geod.s.lon = RADOF(-159.39775555555555555555555555556);
+    agent->cinfo->node.loc.pos.geod.s.h = 348.;
+    agent->cinfo->node.loc.pos.geod.v = gv_zero();
+    agent->cinfo->node.loc.pos.geod.pass++;
+    pos_geod(&agent->cinfo->node.loc);
     if (argc > 3)
     {
         antbase = argv[1];
@@ -257,77 +326,9 @@ int main(int argc, char *argv[])
     else
     {
         printf("Usage: track_antenna antenna mode {file.tle, {date file.tra}, {date x y z vx vy vz}, {az el} [offset_az offset_el]\n");
+        exit(1);
     }
 
-    if (nodename.empty())
-    {
-        agent = new Agent("");
-    }
-    else
-    {
-        agent = new Agent(nodename);
-    }
-
-//    if ((iretn = agent->wait()) < 0)
-//    {
-//        fprintf(agent->get_debug_fd(), "%16.10f %s Failed to start Agent %s on Node %s Dated %s : %s\n",currentmjd(), mjd2iso8601(currentmjd()).c_str(), agent->getAgent().c_str(), agent->getNode().c_str(), utc2iso8601(data_ctime(argv[0])).c_str(), cosmos_error_string(iretn).c_str());
-//        exit(iretn);
-//    }
-//    else
-//    {
-//        fprintf(agent->get_debug_fd(), "%16.10f %s Started Agent %s on Node %s Dated %s\n",currentmjd(), mjd2iso8601(currentmjd()).c_str(), agent->getAgent().c_str(), agent->getNode().c_str(), utc2iso8601(data_ctime(argv[0])).c_str());
-//    }
-
-    iretn = json_createpiece(agent->cinfo, antbase.c_str(), DeviceType::ANT);
-    if (iretn < 0)
-    {
-        fprintf(agent->get_debug_fd(), "Failed to add %s ANT %s\n", antbase.c_str(), cosmos_error_string(iretn).c_str());
-        agent->shutdown();
-        exit(iretn);
-    }
-    devindex = agent->cinfo->pieces[static_cast <uint16_t>(iretn)].cidx;
-    antindex = agent->cinfo->device[devindex].ant.didx;
-    agent->cinfo->device[devindex].ant.minelev = RADOF(10.);
-    if (antbase == "sband")
-    {
-        agent->cinfo->device[devindex].ant.model = DEVICE_MODEL_PRKX2SU;
-    }
-    else if (antbase == "yagi")
-    {
-        agent->cinfo->device[devindex].ant.model = DEVICE_MODEL_GS232B;
-    }
-    else
-    {
-        agent->cinfo->device[devindex].ant.model = DEVICE_MODEL_LOOPBACK;
-    }
-
-    iretn = json_dump_node(agent->cinfo);
-    if (iretn < 0)
-    {
-        fprintf(agent->get_debug_fd(), "Failed to save node %s\n", cosmos_error_string(iretn).c_str());
-        exit(iretn);
-    }
-
-    antdevice = "/dev/tty_" + antbase;
-
-    // Connect to antenna and set sensitivity;
-    iretn = connect_antenna();
-    switch (agent->cinfo->device[devindex].all.model)
-    {
-    case DEVICE_MODEL_GS232B:
-        gs232b_set_sensitivity(RADOF(1.));
-        break;
-    case DEVICE_MODEL_PRKX2SU:
-        prkx2su_set_sensitivity(RADOF(.5));
-        break;
-    }
-
-    agent->cinfo->node.loc.pos.geod.s.lat = RADOF(21.969188888888888888888888888889);
-    agent->cinfo->node.loc.pos.geod.s.lon = RADOF(-159.39775555555555555555555555556);
-    agent->cinfo->node.loc.pos.geod.s.h = 348.;
-    agent->cinfo->node.loc.pos.geod.v = gv_zero();
-    agent->cinfo->node.loc.pos.geod.pass++;
-    pos_geod(&agent->cinfo->node.loc);
 
     ElapsedTime et;
 
