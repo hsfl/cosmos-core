d8431f61c3570426fe7bc6798e1a9513b6d1a605
Jim Lewis <jim_lewis@hotmail.com>
2020-11-05 17:14:05 -0600

    add support for Google Test (gtest) in CMake; add sample unit test driver for cosmosstruc

--- programs/unit_tests/test_cosmosstruc.cpp
+++ programs/unit_tests/test_cosmosstruc.cpp
@@ -1,9 +1,11 @@
 #include "support/configCosmos.h"
-#include "gtest.h"
+#include "test_cosmosstruc.h"
 
-int main(int agrc, char** argv)	{
+int main(int argc, char** argv)	{
 
 	cout<<"Hwllo Eorld!"<<endl;
+
+	run_all_tests(argc, argv);
 
 	return 0;
 }
