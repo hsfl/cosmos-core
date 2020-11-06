01198e10d4ccb6a08bd0ca4946652fe23968da3b
Eric Pilger <ejpilger@gmail.com>
2020-11-06 07:46:05 -1000

    Mixed up error return. This should do it.

--- programs/agents/ground-station/ic9100.cpp
+++ programs/agents/ground-station/ic9100.cpp
@@ -149,10 +149,6 @@ int main(int argc, char *argv[])
         fprintf(agent->get_debug_fd(), "%16.10f %s Failed to start Agent %s on Node %s Dated %s : %s\n",currentmjd(), mjd2iso8601(currentmjd()).c_str(), agent->getAgent().c_str(), agent->getNode().c_str(), utc2iso8601(data_ctime(argv[0])).c_str(), cosmos_error_string(iretn).c_str());
         exit(iretn);
     }
-    else
-    {
-        fprintf(agent->get_debug_fd(), "%16.10f %s Started Agent %s on Node %s Dated %s\n",currentmjd(), mjd2iso8601(currentmjd()).c_str(), agent->getAgent().c_str(), agent->getNode().c_str(), utc2iso8601(data_ctime(argv[0])).c_str());
-    }
 
     if (argc > 6)
     {
@@ -287,7 +283,7 @@ int main(int argc, char *argv[])
         }
         else {
             iretn = ic9100_set_frequency(ic9100, value1);
-            if (iretn >= 0)
+            if (iretn < 0)
             {
                 printf("Unable to set IC9100 Frequency: %s\n", cosmos_error_string(iretn).c_str());
             }
@@ -300,12 +296,12 @@ int main(int argc, char *argv[])
     else if (command == "rfpower")
     {
         iretn = ic9100_get_rfpower(ic9100);
-        if (iretn >= 0)
+        if (iretn < 0)
         {
             printf("Unable to get IC9100 RFPower: %s\n", cosmos_error_string(iretn).c_str());
         }
         else {
-            printf("RFPower: %f\n", ic9100.rfpower);
+            printf("RFPower: %hhu\n", ic9100.rfpower);
         }
     }
 
