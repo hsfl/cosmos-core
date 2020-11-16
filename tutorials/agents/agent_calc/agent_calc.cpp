545904ad4c8cef455b3915c635927d7353c17d27
Jim Lewis <jim_lewis@hotmail.com>
2020-11-16 16:10:37 -0600

    prototype pretty_print functionality for Namespace 2.0

--- tutorials/agents/agent_calc/agent_calc.cpp
+++ tutorials/agents/agent_calc/agent_calc.cpp
@@ -69,6 +69,55 @@ int32_t request_change_node_name(string &request, string &response, Agent *agent
 
 static Agent *agent; // to access the cosmos data, will change later
 
+void replace(std::string& str, const std::string& from, const std::string& to) {
+    if(from.empty())
+        return;
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+    }
+}
+
+vector<size_t> find_newlines(string sample)
+{
+    vector<size_t> characterLocations;
+    for(size_t i =0; i < sample.size(); i++) if(sample[i] == '\n') characterLocations.push_back(i);
+    return characterLocations;
+}
+
+void pretty_print(string& js)	{
+
+	replace(js, ", ", ",\n");
+	replace(js, "{", "{\n");
+	replace(js, "[", "[\n");
+	replace(js, "}", "\n}");
+	replace(js, "]", "\n]");
+
+	// create vector[char position] = # of indents
+	int indent = 0;
+	vector<size_t> indents;
+	for(size_t i = 0; i < js.size(); ++i)	{
+		if(js[i]=='['){	++indent;}
+		if(js[i]=='{'){	++indent;}
+		if(js[i]==']'){	--indent; indents[i-1]--;}
+		if(js[i]=='}'){	--indent; indents[i-1]--;}
+		indents.push_back(indent);
+	}
+
+	// find position of all '\n' characters
+	vector<size_t> newlines = find_newlines(js);
+
+	// insert the appropriate # of indentations after the '\n' char
+    for(size_t i = newlines.size(); --i!=0; ) {
+		string indent_string;
+		for(size_t j = 0; j < indents[newlines[i]]; ++j)	indent_string += "  ";
+		js.insert(newlines[i]+1, indent_string);
+	}
+	return;
+}
+
+
 int main(int argc, char *argv[])
 {
     int iretn;
@@ -157,12 +206,27 @@ int main(int argc, char *argv[])
 
 
 
-string entire = R"({
-"Entire COSMOSSTRUC": {
-	"node": {"name": "HAL12001", "downtime" : 123},
+
+
+string entire = R"(
+{
+  "Entire COSMOSSTRUC": {
+	"node": {
+		"name": "HAL12001",
+		"downtime" : 123
+	},
 	"jmapped": 181,
 	"unit": [
-		[{"name": "", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "m", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "km", "p0": 0, "p1": 0.0010000000474974513, "p2": 0, "type": 1}, {"name": "cm", "p0": 0, "p1": 100, "p2": 0, "type": 1}, {"name": "ft", "p0": 0, "p1": 3.2808330059051514, "p2": 0, "type": 1}], [{"name": "kg", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "g", "p0": 0, "p1": 1000, "p2": 0, "type": 1}], [{"name": "kgm2", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "s", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "min", "p0": 0, "p1": 0.01666666753590107, "p2": 0, "type": 1}, {"name": "hr", "p0": 0, "p1": 0.00027777778450399637, "p2": 0, "type": 1}, {"name": "day", "p0": 0, "p1": 1.1574074051168282e-05, "p2": 0, "type": 1}, {"name": "MJD", "p0": 0, "p1": 1.1574074051168282e-05, "p2": 0, "type": 1}], [{"name": "MJD", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "min", "p0": 0, "p1": 1440, "p2": 0, "type": 1}, {"name": "hr", "p0": 0, "p1": 24, "p2": 0, "type": 1}, {"name": "day", "p0": 0, "p1": 1, "p2": 0, "type": 1}], [{"name": "A", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "K", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "�C", "p0": -273.14999389648438, "p1": 1, "p2": 0, "type": 1}], [{"name": "Ca", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "m2", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "ft2", "p0": 0, "p1": 10.763910293579102, "p2": 0, "type": 1}], [{"name": "m3", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "m/s", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "km/s", "p0": 0, "p1": 0.0010000000474974513, "p2": 0, "type": 1}, {"name": "cm/s", "p0": 0, "p1": 100, "p2": 0, "type": 1}], [{"name": "m/s2", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "g", "p0": 0, "p1": 0.10197161883115768, "p2": 0, "type": 1}], [{"name": "kg/m3", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "Cd/m2", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "rad", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "deg", "p0": 0, "p1": 57.295780181884766, "p2": 0, "type": 1}], [{"name": "rad/s", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "deg/s", "p0": 0, "p1": 57.295780181884766, "p2": 0, "type": 1}], [{"name": "", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "sr", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "Hz", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "N", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "dyn", "p0": 0, "p1": 100000, "p2": 0, "type": 1}, {"name": "lb", "p0": 0, "p1": 0.22481000423431396, "p2": 0, "type": 1}], [{"name": "Nm", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "Pa", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "j", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "erg", "p0": 0, "p1": 10000000, "p2": 0, "type": 1}], [{"name": "watt", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "C", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "Ahr", "p0": 0, "p1": 3600, "p2": 0, "type": 1}], [{"name": "V", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "F", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "ohm", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "A/m", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "Wb", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "T", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "uT", "p0": 0, "p1": 1000000, "p2": 0, "type": 1}, {"name": "nT", "p0": 0, "p1": 1000000000, "p2": 0, "type": 1}], [{"name": "Am2", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "s", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "GiB", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "KiB", "p0": 0, "p1": 0.0009765625, "p2": 0, "type": 1}], [{"name": "", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "Please work", "p0": 0, "p2": 0, "type": 13}]],
+		[
+			{
+				"name": "",
+				"p0": 0,
+				"p1": 0,
+				"p2": 0,
+				"type": 0
+			}
+		],
+		[{"name": "m", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "km", "p0": 0, "p1": 0.0010000000474974513, "p2": 0, "type": 1}, {"name": "cm", "p0": 0, "p1": 100, "p2": 0, "type": 1}, {"name": "ft", "p0": 0, "p1": 3.2808330059051514, "p2": 0, "type": 1}], [{"name": "kg", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "g", "p0": 0, "p1": 1000, "p2": 0, "type": 1}], [{"name": "kgm2", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "s", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "min", "p0": 0, "p1": 0.01666666753590107, "p2": 0, "type": 1}, {"name": "hr", "p0": 0, "p1": 0.00027777778450399637, "p2": 0, "type": 1}, {"name": "day", "p0": 0, "p1": 1.1574074051168282e-05, "p2": 0, "type": 1}, {"name": "MJD", "p0": 0, "p1": 1.1574074051168282e-05, "p2": 0, "type": 1}], [{"name": "MJD", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "min", "p0": 0, "p1": 1440, "p2": 0, "type": 1}, {"name": "hr", "p0": 0, "p1": 24, "p2": 0, "type": 1}, {"name": "day", "p0": 0, "p1": 1, "p2": 0, "type": 1}], [{"name": "A", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "K", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "�C", "p0": -273.14999389648438, "p1": 1, "p2": 0, "type": 1}], [{"name": "Ca", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "m2", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "ft2", "p0": 0, "p1": 10.763910293579102, "p2": 0, "type": 1}], [{"name": "m3", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "m/s", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "km/s", "p0": 0, "p1": 0.0010000000474974513, "p2": 0, "type": 1}, {"name": "cm/s", "p0": 0, "p1": 100, "p2": 0, "type": 1}], [{"name": "m/s2", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "g", "p0": 0, "p1": 0.10197161883115768, "p2": 0, "type": 1}], [{"name": "kg/m3", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "Cd/m2", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "rad", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "deg", "p0": 0, "p1": 57.295780181884766, "p2": 0, "type": 1}], [{"name": "rad/s", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "deg/s", "p0": 0, "p1": 57.295780181884766, "p2": 0, "type": 1}], [{"name": "", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "sr", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "Hz", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "N", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "dyn", "p0": 0, "p1": 100000, "p2": 0, "type": 1}, {"name": "lb", "p0": 0, "p1": 0.22481000423431396, "p2": 0, "type": 1}], [{"name": "Nm", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "Pa", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "j", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "erg", "p0": 0, "p1": 10000000, "p2": 0, "type": 1}], [{"name": "watt", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "C", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "Ahr", "p0": 0, "p1": 3600, "p2": 0, "type": 1}], [{"name": "V", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "F", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "ohm", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "A/m", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "Wb", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "T", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "uT", "p0": 0, "p1": 1000000, "p2": 0, "type": 1}, {"name": "nT", "p0": 0, "p1": 1000000000, "p2": 0, "type": 1}], [{"name": "Am2", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "s", "p0": 0, "p1": 0, "p2": 0, "type": 0}], [{"name": "GiB", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "KiB", "p0": 0, "p1": 0.0009765625, "p2": 0, "type": 1}], [{"name": "", "p0": 0, "p1": 0, "p2": 0, "type": 0}, {"name": "Please work", "p0": 0, "p2": 0, "type": 13}]],
 "equation": [
 	{"name": "string3", "value": "string4"}
 ]
@@ -173,8 +237,10 @@ string entire = R"({
 
 agent->cinfo->set_json_value<cosmosstruc>("Entire COSMOSSTRUC", entire);
 
-	cout<<"Output:\n\t<"<<agent->cinfo->get_json<cosmosstruc>("Entire COSMOSSTRUC")<<">"<<endl;
-
+	//cout<<"Output:\n\t<"<<agent->cinfo->get_json<cosmosstruc>("Entire COSMOSSTRUC")<<">"<<endl;
+	string js = agent->cinfo->get_json<cosmosstruc>("Entire COSMOSSTRUC");	
+	pretty_print(js);
+	cout<<"Output:<\n"<<js<<"\n>"<<endl;
 /*
 
 	agent->cinfo->add_name("Entire COSMOSSTRUC Unit", &agent->cinfo->unit);
