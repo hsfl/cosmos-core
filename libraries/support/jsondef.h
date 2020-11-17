77211b7448243690ff424a1c90641b3f9e56c87b
Jim Lewis <jim_lewis@hotmail.com>
2020-11-17 09:32:36 -0600

    support for JSON pretty print!

--- libraries/support/jsondef.h
+++ libraries/support/jsondef.h
@@ -3073,6 +3073,49 @@ struct cosmosstruc
 		}
 	}
 
+	void replace(std::string& str, const std::string& from, const std::string& to) {
+    	if(from.empty()) return;
+    	size_t start_pos = 0;
+    	while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        	str.replace(start_pos, from.length(), to);
+        	start_pos += to.length();
+    	}
+    	return;
+	}
+	
+	void pretty_form(string& js)    {
+	
+    	replace(js, ", ", ",\n");
+    	replace(js, "{", "{\n");
+    	replace(js, "[", "[\n");
+    	replace(js, "}", "\n}");
+    	replace(js, "]", "\n]");
+	
+    	// create vector[char position] = # of indents
+    	int indent = 0;
+    	vector<size_t> indents;
+    	for(size_t i = 0; i < js.size(); ++i)   {
+        	if(js[i]=='['){ ++indent;}
+        	if(js[i]=='{'){ ++indent;}
+        	if(js[i]==']'){ --indent; indents[i-1]--;}
+        	if(js[i]=='}'){ --indent; indents[i-1]--;}
+        	indents.push_back(indent);
+    	}
+		
+    	// find position of all '\n' characters
+    	vector<size_t> newlines;
+    	for(size_t i =0; i < js.size(); i++) if(js[i] == '\n') newlines.push_back(i);
+	
+    	// insert the appropriate # of indents after the '\n' char
+    	for(size_t i = newlines.size(); i!=0; ) {
+        	--i;
+        	string indent_string;
+        	for(size_t j = 0; j < indents[newlines[i]]; ++j)    indent_string += "  ";
+        	js.insert(newlines[i]+1, indent_string);
+    	}
+    	return;
+	}
+
 	template<class T>
 	string get_json_pretty(const string& s)	{
 		if(name_exists(s))	{
@@ -3108,12 +3151,11 @@ struct cosmosstruc
  			for(size_t i = 0; i < unit.size(); ++i)	{
  				for(size_t j = 0; j < unit[i].size(); ++j)	{
 					// SCOTTNOTE: add is_null check here
- 					unit[i][j].from_json(p[obj]["unit"][i][j].dump());
+ 					if(!p[obj]["unit"][i][j].is_null())	unit[i][j].from_json(p[obj]["unit"][i][j].dump());
  				}
  			}
  			for(size_t i = 0; i < equation.size(); ++i)	{
-				// SCOTTNOTE: add is_null check here
- 				equation[i].from_json(p[obj]["equation"][i].dump());
+ 				if(!p[obj]["equation"][i].is_null())	equation[i].from_json(p[obj]["equation"][i].dump());
 			}
  			if(!p[obj]["node"].is_null())	node.from_json(p[obj]["node"].dump());
 		} else {
