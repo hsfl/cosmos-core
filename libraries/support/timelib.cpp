8b8bcc7e95c761a0bfffad32ffa7f45ad17a03bb
Eric Pilger <ejpilger@gmail.com>
2020-11-23 09:28:24 -1000

    Fixed "60 seconds" problem

--- libraries/support/timelib.cpp
+++ libraries/support/timelib.cpp
@@ -1296,7 +1296,7 @@ string utc2iso8601(double utc)
     fd -= ihh / 24.;
     imm = (int32_t)(1440 * fd);
     fd -= imm / 1440.;
-    iss = (int32_t)(86400 * fd + .5);
+    iss = (int32_t)(86400 * fd);
     sprintf(buffer, "%04d-%02d-%02dT%02d:%02d:%02d", iy, im, id, ihh, imm, iss);
 
     return string(buffer);
