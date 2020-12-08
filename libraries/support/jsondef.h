18bad547b6bd9f41c55ea190c573b24b13c82292
Jim Lewis <jim_lewis@hotmail.com>
2020-12-08 16:54:47 -0600

    fix indents

--- libraries/support/jsondef.h
+++ libraries/support/jsondef.h
@@ -6120,281 +6120,279 @@ struct cosmosstruc
         return;
     }
 
-// has left association?
-bool left(char a)	{
-	if(a == '+')	return true;
-	if(a == '-')	return true;
-	if(a == '*')	return true;
-	if(a == '/')	return true;
-	if(a == '^')	return false;
+	// has left association?
+	bool left(char a)	{
+		if(a == '+')	return true;
+		if(a == '-')	return true;
+		if(a == '*')	return true;
+		if(a == '/')	return true;
+		if(a == '^')	return false;
 
-	return false;
-}
-
-
-// has equal precedence?
-bool equal(char a, char b)	{
-	if(a == '+' && b == '+')	return true;	
-	if(a == '+' && b == '-')	return true;	
-	if(a == '+' && b == '*')	return false;	
-	if(a == '+' && b == '/')	return false;	
-	if(a == '+' && b == '^')	return false;	
-	
-	if(a == '-' && b == '+')	return true;	
-	if(a == '-' && b == '-')	return true;	
-	if(a == '-' && b == '*')	return false;	
-	if(a == '-' && b == '/')	return false;	
-	if(a == '-' && b == '^')	return false;	
-	
-	if(a == '*' && b == '+')	return false;	
-	if(a == '*' && b == '-')	return false;	
-	if(a == '*' && b == '*')	return true;	
-	if(a == '*' && b == '/')	return true;	
-	if(a == '*' && b == '^')	return false;	
-	
-	if(a == '/' && b == '+')	return false;	
-	if(a == '/' && b == '-')	return false;	
-	if(a == '/' && b == '*')	return true;	
-	if(a == '/' && b == '/')	return true;	
-	if(a == '/' && b == '^')	return false;	
-	
-	if(a == '^' && b == '+')	return false;	
-	if(a == '^' && b == '-')	return false;	
-	if(a == '^' && b == '*')	return false;	
-	if(a == '^' && b == '/')	return false;	
-	if(a == '^' && b == '^')	return true;	
-
-	return false;
-}
-
-// has higher precedence?
-bool higher(char a, char b)	{
-
-	if(a == '+' && b == '+')	return false;	
-	if(a == '+' && b == '-')	return false;	
-	if(a == '+' && b == '*')	return false;	
-	if(a == '+' && b == '/')	return false;	
-	if(a == '+' && b == '^')	return false;	
-	
-	if(a == '-' && b == '+')	return false;	
-	if(a == '-' && b == '-')	return false;	
-	if(a == '-' && b == '*')	return false;	
-	if(a == '-' && b == '/')	return false;	
-	if(a == '-' && b == '^')	return false;	
-	
-	if(a == '*' && b == '+')	return true;	
-	if(a == '*' && b == '-')	return true;	
-	if(a == '*' && b == '*')	return false;	
-	if(a == '*' && b == '/')	return false;	
-	if(a == '*' && b == '^')	return false;	
-	
-	if(a == '/' && b == '+')	return true;	
-	if(a == '/' && b == '-')	return true;	
-	if(a == '/' && b == '*')	return false;	
-	if(a == '/' && b == '/')	return false;	
-	if(a == '/' && b == '^')	return false;	
-	
-	if(a == '^' && b == '+')	return true;	
-	if(a == '^' && b == '-')	return true;	
-	if(a == '^' && b == '*')	return true;	
-	if(a == '^' && b == '/')	return true;	
-	if(a == '^' && b == '^')	return false;	
-
-	return false;
-}
-
-int apply_op(stack<char>& ops, stack<double>& answer)	{
-	if(answer.size()<2) return -1;
-	double b = answer.top();
-	answer.pop();
-	double a = answer.top();
-	answer.pop();
-	switch(ops.top())	{
-		case '+':	answer.push(a+b);
-					break;
-		case '-':	answer.push(a-b);
-					break;
-		case '*':	answer.push(a*b);
-					break;
-		case '/':	answer.push(a/b);
-					break;
-		case '^':	answer.push(pow(a,b));
-					break;
+		return false;
 	}
-	//cout<<"		calculating "<<a<<" "<<ops.top()<<" "<<b<<" = "<<answer.top()<<endl;
-	ops.pop();
-	return 0;
-}
 
-// TODO:  make sure it never segfaults!
-double equationator(const string& str)	{
-	string eq(str);
+	// has equal precedence?
+	bool equal(char a, char b)	{
+		if(a == '+' && b == '+')	return true;	
+		if(a == '+' && b == '-')	return true;	
+		if(a == '+' && b == '*')	return false;	
+		if(a == '+' && b == '/')	return false;	
+		if(a == '+' && b == '^')	return false;	
 
-// START EQUATION PRE-PROCESSING
+		if(a == '-' && b == '+')	return true;	
+		if(a == '-' && b == '-')	return true;	
+		if(a == '-' && b == '*')	return false;	
+		if(a == '-' && b == '/')	return false;	
+		if(a == '-' && b == '^')	return false;	
 
-	// check if empty
-	if(eq.empty())	return nan("");
+		if(a == '*' && b == '+')	return false;	
+		if(a == '*' && b == '-')	return false;	
+		if(a == '*' && b == '*')	return true;	
+		if(a == '*' && b == '/')	return true;	
+		if(a == '*' && b == '^')	return false;	
 
-	// check if double quotes are balanced
-	int q_count = 0;
-	for(std::string::const_iterator it = eq.begin(); it != eq.end(); ++it) {
-		if(*it=='"')	q_count++;
+		if(a == '/' && b == '+')	return false;	
+		if(a == '/' && b == '-')	return false;	
+		if(a == '/' && b == '*')	return true;	
+		if(a == '/' && b == '/')	return true;	
+		if(a == '/' && b == '^')	return false;	
+
+		if(a == '^' && b == '+')	return false;	
+		if(a == '^' && b == '-')	return false;	
+		if(a == '^' && b == '*')	return false;	
+		if(a == '^' && b == '/')	return false;	
+		if(a == '^' && b == '^')	return true;	
+
+		return false;
 	}
-	if(q_count%2==1)	return nan("");
 
-	// TODO: you should never have #( or )#...  implied multiplication, but make explicit already!
-	// trim leading whitespace
- 	const auto notwhite = eq.find_first_not_of(" \n\r\t\f\v");
-	eq = eq.substr(notwhite);
+	// has higher precedence?
+	bool higher(char a, char b)	{
 
-	// replace "cosmos_variable_names" with values
-	vector<string> replacements;
-	for(size_t i = 0; i < eq.size(); ++i)	{
-		//cout<<"char = <"<<eq[i]<<">"<<endl;
-		if(eq[i]=='"')	{
-			string name("");
-			//cout<<"found opening quote"<<endl;
-			while(eq[++i]!='"'&&i<eq.size())	{ name.push_back(eq[i]); }
-			//cout<<"name = "<<name<<endl;
-			replacements.push_back(name);
+		if(a == '+' && b == '+')	return false;	
+		if(a == '+' && b == '-')	return false;	
+		if(a == '+' && b == '*')	return false;	
+		if(a == '+' && b == '/')	return false;	
+		if(a == '+' && b == '^')	return false;	
+
+		if(a == '-' && b == '+')	return false;	
+		if(a == '-' && b == '-')	return false;	
+		if(a == '-' && b == '*')	return false;	
+		if(a == '-' && b == '/')	return false;	
+		if(a == '-' && b == '^')	return false;	
+
+		if(a == '*' && b == '+')	return true;	
+		if(a == '*' && b == '-')	return true;	
+		if(a == '*' && b == '*')	return false;	
+		if(a == '*' && b == '/')	return false;	
+		if(a == '*' && b == '^')	return false;	
+
+		if(a == '/' && b == '+')	return true;	
+		if(a == '/' && b == '-')	return true;	
+		if(a == '/' && b == '*')	return false;	
+		if(a == '/' && b == '/')	return false;	
+		if(a == '/' && b == '^')	return false;	
+
+		if(a == '^' && b == '+')	return true;	
+		if(a == '^' && b == '-')	return true;	
+		if(a == '^' && b == '*')	return true;	
+		if(a == '^' && b == '/')	return true;	
+		if(a == '^' && b == '^')	return false;	
+
+		return false;
+	}
+
+	int apply_op(stack<char>& ops, stack<double>& answer)	{
+		if(answer.size()<2) return -1;
+		double b = answer.top();
+		answer.pop();
+		double a = answer.top();
+		answer.pop();
+		switch(ops.top())	{
+			case '+':	answer.push(a+b);
+						break;
+			case '-':	answer.push(a-b);
+						break;
+			case '*':	answer.push(a*b);
+						break;
+			case '/':	answer.push(a/b);
+						break;
+			case '^':	answer.push(pow(a,b));
+						break;
 		}
+		//cout<<"		calculating "<<a<<" "<<ops.top()<<" "<<b<<" = "<<answer.top()<<endl;
+		ops.pop();
+		return 0;
 	}
 
-	for(size_t i = 0; i < replacements.size(); ++i)	{
-		string replace_me = "\"" + replacements[i] + "\"";
-		string type = get_type(replacements[i]);
-		if(type=="double")	{
-			replace(eq, replace_me, to_string(get_value<double>(replacements[i])));
-		} else if(type=="float")	{
-			replace(eq, replace_me, to_string(get_value<float>(replacements[i])));
-		} else if(type=="size_t")	{
-			replace(eq, replace_me, to_string(get_value<size_t>(replacements[i])));
-		} else if(type=="int")	{
-			replace(eq, replace_me, to_string(get_value<int>(replacements[i])));
-		} else if(type=="uint16_t")	{
-			replace(eq, replace_me, to_string(get_value<uint16_t>(replacements[i])));
-		} else if(type=="int16_t")	{
-			replace(eq, replace_me, to_string(get_value<int16_t>(replacements[i])));
-		} else if(type=="uint32_t")	{
-			replace(eq, replace_me, to_string(get_value<uint32_t>(replacements[i])));
-		} else if(type=="int32_t")	{
-			replace(eq, replace_me, to_string(get_value<int32_t>(replacements[i])));
-		} else if(type=="uint8_t")	{
-			replace(eq, replace_me, to_string(get_value<uint8_t>(replacements[i])));
-		} else if(type=="int8_t")	{
-			replace(eq, replace_me, to_string(get_value<int8_t>(replacements[i])));
-		} else if(type=="bool")	{
-			replace(eq, replace_me, to_string(get_value<bool>(replacements[i])));
-		} else	{
-			cout<<"type <"<<type<<"> for <"<<replacements[i]<<"> not supported"<<endl;
-			return nan("");
+	// TODO:  make sure it never segfaults!
+	double equationator(const string& str)	{
+		string eq(str);
+
+	// START EQUATION PRE-PROCESSING
+
+		// check if empty
+		if(eq.empty())	return nan("");
+
+		// check if double quotes are balanced
+		int q_count = 0;
+		for(std::string::const_iterator it = eq.begin(); it != eq.end(); ++it) {
+			if(*it=='"')	q_count++;
 		}
-	}
+		if(q_count%2==1)	return nan("");
+
+		// TODO: you should never have #( or )#...  implied multiplication, but make explicit already!
+		// trim leading whitespace
+ 		const auto notwhite = eq.find_first_not_of(" \n\r\t\f\v");
+		eq = eq.substr(notwhite);
+
+		// replace "cosmos_variable_names" with values
+		vector<string> replacements;
+		for(size_t i = 0; i < eq.size(); ++i)	{
+			//cout<<"char = <"<<eq[i]<<">"<<endl;
+			if(eq[i]=='"')	{
+				string name("");
+				//cout<<"found opening quote"<<endl;
+				while(eq[++i]!='"'&&i<eq.size())	{ name.push_back(eq[i]); }
+				//cout<<"name = "<<name<<endl;
+				replacements.push_back(name);
+			}
+		}
+
+		for(size_t i = 0; i < replacements.size(); ++i)	{
+			string replace_me = "\"" + replacements[i] + "\"";
+			string type = get_type(replacements[i]);
+			if(type=="double")	{
+				replace(eq, replace_me, to_string(get_value<double>(replacements[i])));
+			} else if(type=="float")	{
+				replace(eq, replace_me, to_string(get_value<float>(replacements[i])));
+			} else if(type=="size_t")	{
+				replace(eq, replace_me, to_string(get_value<size_t>(replacements[i])));
+			} else if(type=="int")	{
+				replace(eq, replace_me, to_string(get_value<int>(replacements[i])));
+			} else if(type=="uint16_t")	{
+				replace(eq, replace_me, to_string(get_value<uint16_t>(replacements[i])));
+			} else if(type=="int16_t")	{
+				replace(eq, replace_me, to_string(get_value<int16_t>(replacements[i])));
+			} else if(type=="uint32_t")	{
+				replace(eq, replace_me, to_string(get_value<uint32_t>(replacements[i])));
+			} else if(type=="int32_t")	{
+				replace(eq, replace_me, to_string(get_value<int32_t>(replacements[i])));
+			} else if(type=="uint8_t")	{
+				replace(eq, replace_me, to_string(get_value<uint8_t>(replacements[i])));
+			} else if(type=="int8_t")	{
+				replace(eq, replace_me, to_string(get_value<int8_t>(replacements[i])));
+			} else if(type=="bool")	{
+				replace(eq, replace_me, to_string(get_value<bool>(replacements[i])));
+			} else	{
+				cout<<"type <"<<type<<"> for <"<<replacements[i]<<"> not supported"<<endl;
+				return nan("");
+			}
+		}
 	
-	// replace {}[] with ()
-	replace(eq, "{", "(");
-	replace(eq, "[", "(");
-	replace(eq, "}", ")");
-	replace(eq, "]", ")");
+		// replace {}[] with ()
+		replace(eq, "{", "(");
+		replace(eq, "[", "(");
+		replace(eq, "}", ")");
+		replace(eq, "]", ")");
 	
-	// check if parenthesis are balanced
-	int p_count = 0;
-	for(std::string::const_iterator it = eq.begin(); it != eq.end(); ++it) {
-		if(*it=='(')	p_count++;
-		if(*it==')')	p_count--;
-		if(p_count<0)	return nan("");
-	}
-	if(p_count!=0)	return nan("");
+		// check if parenthesis are balanced
+		int p_count = 0;
+		for(std::string::const_iterator it = eq.begin(); it != eq.end(); ++it) {
+			if(*it=='(')	p_count++;
+			if(*it==')')	p_count--;
+			if(p_count<0)	return nan("");
+		}
+		if(p_count!=0)	return nan("");
 
 
-// START EQUATION PROCESSING
+	// START EQUATION PROCESSING
 
-	string output;
-	stack<double> answer;
-	stack<char> ops;
-				//int count = 0;
-	for(std::string::const_iterator it = eq.begin(); it != eq.end(); ++it) {
-				// debug
-				// cout<<"char #"<<count++<<" = '"<<*it<<"' :\n\t<"<<output<<">"<<endl;
-				// cout<<"\t: operators = <";
-				// for(stack<char> op = ops; !op.empty(); op.pop())	{ cout<<op.top()<<" "; }
-					// cout<<">"<<endl;
+		string output;
+		stack<double> answer;
+		stack<char> ops;
+					//int count = 0;
+		for(std::string::const_iterator it = eq.begin(); it != eq.end(); ++it) {
+					// debug
+					// cout<<"char #"<<count++<<" = '"<<*it<<"' :\n\t<"<<output<<">"<<endl;
+					// cout<<"\t: operators = <";
+					// for(stack<char> op = ops; !op.empty(); op.pop())	{ cout<<op.top()<<" "; }
+						// cout<<">"<<endl;
 
-		// all letters should have been replaced by values
-		if(isalpha(*it))	return nan("");
-		// skip all whitespace
-		if(isspace(*it))	continue;
-		// if token is number
-		if(isdigit(*it)||*it=='.')	{
-			bool negative = false;
-			if(*(it-1)=='+'||*(it-1)=='-')	{
-				string::const_iterator iit = it-1;
-				if(iit==eq.begin())	{
-					if(*(it-1)=='-')	negative = true;
-					ops.pop();
-				} else {
-					while(iit--!=eq.begin())	{
-						if(isspace(*iit))	continue;
-						if(*iit=='('||*iit=='+'||*iit=='-'||*iit=='*'||*iit=='/'||*iit=='^')	{
-							if(*(it-1)=='-')	negative = true;
-							ops.pop();
-							break;
-						} else	{
-							break;
-						}
-					}
-					if(iit==eq.begin() && (*iit==' '||*iit=='\t'||*iit=='\n'))	{
+			// all letters should have been replaced by values
+			if(isalpha(*it))	return nan("");
+			// skip all whitespace
+			if(isspace(*it))	continue;
+			// if token is number
+			if(isdigit(*it)||*it=='.')	{
+				bool negative = false;
+				if(*(it-1)=='+'||*(it-1)=='-')	{
+					string::const_iterator iit = it-1;
+					if(iit==eq.begin())	{
 						if(*(it-1)=='-')	negative = true;
 						ops.pop();
+					} else {
+						while(iit--!=eq.begin())	{
+							if(isspace(*iit))	continue;
+							if(*iit=='('||*iit=='+'||*iit=='-'||*iit=='*'||*iit=='/'||*iit=='^')	{
+								if(*(it-1)=='-')	negative = true;
+								ops.pop();
+								break;
+							} else	{
+								break;
+							}
+						}
+						if(iit==eq.begin() && (*iit==' '||*iit=='\t'||*iit=='\n'))	{
+							if(*(it-1)=='-')	negative = true;
+							ops.pop();
+						}
 					}
 				}
-			}
-			vector<int> integer, fraction;
-			if(isdigit(*it))	{
-				integer.push_back(*it-'0');
-			} else {
-				integer.push_back(0); --it;
-			}
-			while(isdigit(*(it+1)))	{ integer.push_back(*(++it)-'0'); }
-			if(*(it+1)=='.')	{ ++it; while(isdigit(*(it+1)))	{ fraction.push_back(*(++it)-'0'); } }
-			double numnum = 0.;
-			for(size_t i = 0; i < integer.size(); ++i)	{ numnum += integer[i]*1.0 * pow(10, integer.size()-i-1); }
-			for(size_t i = 0; i < fraction.size(); ++i)	{ numnum += fraction[i]*1.0 * pow(10.0, -(i+1.0)); }
-			if(negative) numnum *= -1.;
-			stringstream ss;
-			ss<<std::setprecision(std::numeric_limits<double>::digits10)<<numnum;
-			output += ss.str() + " ";
-			answer.push(numnum);
-			continue;
-		}
-		// if token is operator
-		if(*it=='+'||*it=='-'||*it=='*'||*it=='/'||*it=='^')	{
-			if((*it=='+'||*it=='-')&&(isdigit(*(it+1))||*(it+1)=='.'))	{
-				if(it==eq.begin())	{ ops.push(*it); continue; }
-				string::const_iterator t = it-1;
-				while(t!=eq.begin() && (*t==' '||*t=='\n'||*t=='\t')) --t;
-				if(*t=='+'||*t=='-'||*t=='*'||*t=='/'||*t=='^')	{
-					// only gently pushed, will be popped when negative number found
-					ops.push(*it);
-					continue;
+				vector<int> integer, fraction;
+				if(isdigit(*it))	{
+					integer.push_back(*it-'0');
+				} else {
+					integer.push_back(0); --it;
 				}
+				while(isdigit(*(it+1)))	{ integer.push_back(*(++it)-'0'); }
+				if(*(it+1)=='.')	{ ++it; while(isdigit(*(it+1)))	{ fraction.push_back(*(++it)-'0'); } }
+				double numnum = 0.;
+				for(size_t i = 0; i < integer.size(); ++i)	{ numnum += integer[i]*1.0 * pow(10, integer.size()-i-1); }
+				for(size_t i = 0; i < fraction.size(); ++i)	{ numnum += fraction[i]*1.0 * pow(10.0, -(i+1.0)); }
+				if(negative) numnum *= -1.;
+				stringstream ss;
+				ss<<std::setprecision(std::numeric_limits<double>::digits10)<<numnum;
+				output += ss.str() + " ";
+				answer.push(numnum);
+				continue;
 			}
-			while(	!ops.empty() &&
-					( higher(ops.top(), *it) || (equal(ops.top(), *it) && left(*it)) ) &&
-					ops.top()!='('
-			)	{ output += string(1,(*it)) + " "; if(apply_op(ops, answer)<0) return nan(""); }
-			ops.push(*it);
-		} else if(*it == '(')	{
-			ops.push(*it);
-		} else if(*it == ')')	{
-			while(ops.top()!='(')	{ output += string(1,(*it)) + " "; if(apply_op(ops, answer)<0) return nan(""); }
-			if(ops.top()=='(')	{ if(ops.empty()) return nan(""); else ops.pop(); }
+			// if token is operator
+			if(*it=='+'||*it=='-'||*it=='*'||*it=='/'||*it=='^')	{
+				if((*it=='+'||*it=='-')&&(isdigit(*(it+1))||*(it+1)=='.'))	{
+					if(it==eq.begin())	{ ops.push(*it); continue; }
+					string::const_iterator t = it-1;
+					while(t!=eq.begin() && (*t==' '||*t=='\n'||*t=='\t')) --t;
+					if(*t=='+'||*t=='-'||*t=='*'||*t=='/'||*t=='^')	{
+						// only gently pushed, will be popped when negative number found
+						ops.push(*it);
+						continue;
+					}
+				}
+				while(	!ops.empty() &&
+						( higher(ops.top(), *it) || (equal(ops.top(), *it) && left(*it)) ) &&
+						ops.top()!='('
+				)	{ output += string(1,(*it)) + " "; if(apply_op(ops, answer)<0) return nan(""); }
+				ops.push(*it);
+			} else if(*it == '(')	{
+				ops.push(*it);
+			} else if(*it == ')')	{
+				while(ops.top()!='(')	{ output += string(1,(*it)) + " "; if(apply_op(ops, answer)<0) return nan(""); }
+				if(ops.top()=='(')	{ if(ops.empty()) return nan(""); else ops.pop(); }
+			}
 		}
+		while(!ops.empty())	{ output += string(1,ops.top()) + " "; if(apply_op(ops, answer)<0) return nan(""); }
+		return answer.top();
 	}
-	while(!ops.empty())	{ output += string(1,ops.top()) + " "; if(apply_op(ops, answer)<0) return nan(""); }
-	return answer.top();
-}
-
 
         // other namespace member functions??
         // maybe set_json for use with namespace names (calls from_json...)
