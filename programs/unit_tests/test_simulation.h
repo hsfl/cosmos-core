e50b21d3f394d4c275fcbc1dac380223f9a4aea0
ginoscott <ginoscottdev@gmail.com>
2021-02-04 23:58:06 -1000

    sending reqs to all agents, get_initial_time

--- programs/unit_tests/test_simulation.h
+++ programs/unit_tests/test_simulation.h
@@ -2,6 +2,7 @@
 #define __TEST_SIMULATION
 
 #include <math.h>
+#include <vector>
 
 #include "gtest/gtest.h"
 #include "agent/agentclass.h"
@@ -12,12 +13,12 @@ static Agent* agent;
 // (for a single agent)
 // Make a request for the agent states
 TEST(simulation, request_agent_states) {
-	agent = new Agent("world","controller",1.);
-	simulation sim(agent);
-	ASSERT_TRUE(sim.all_sim_agents_running());
+	//agent = new Agent("world","controller",1.);
+	
 }
 
 // HCL happens here
+// ie: time coordinating the state vectors
 TEST(simulation, hcl) {
 	
 }
@@ -34,9 +35,25 @@ TEST(simulation, sph_output) {
 
 
 // (for the simulation)
+// Sim agent role call
+TEST(simulation, all_sim_agents_running) {
+	/*agent = new Agent("world","controller",1.);
+	simulation sim(agent);
+	ASSERT_TRUE(sim.all_sim_agents_running());*/
+}
 // Start agents at not really random substeps of dt
 TEST(simulation, start_agents) {
-	
+	agent = new Agent("world","controller",1.);
+	simulation sim(agent);
+	sim.init_sim_agents();
+	std::vector<string> response = sim.send_req_to_all_agents("get_initial_time");
+	std::vector<double> initial_times(response.size());
+	std::transform(response.begin(), response.end(), initial_times.begin(), [](const std::string& val) {
+    	return std::stod(val);
+	});
+	for(size_t i = 0; i < initial_times.size(); ++i) {
+		ASSERT_DOUBLE_EQ(initial_times[i], 0);
+	}
 }
 
 // Populate and calculate world history
