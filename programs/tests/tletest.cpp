494eba8d2c58bbab481cbe88e3b2dfd05a0c2f28
Eric Pilger <ejpilger@gmail.com>
2021-03-20 12:28:16 -1000

    Now demonstrates the problem we need to work on.

--- programs/tests/tletest.cpp
+++ programs/tests/tletest.cpp
@@ -17,12 +17,14 @@ int main(int argc, char *argv[])
     }
 
     tlestruc tle2;
-    eci2tle(loc.pos.eci.utc, loc.pos.eci, tle2);
-
     locstruc loc2;
-    tle2eci(tle2.utc, tle2, loc2.pos.eci);
+    eci2tle(loc.pos.eci.utc, loc.pos.eci, tle2);
+    for (double dt=0.; dt<1.; dt+=.1)
+    {
+        lines2eci(lines[0].utc+dt, lines, loc.pos.eci);
+        tle2eci(tle2.utc+dt, tle2, loc2.pos.eci);
 
-    printf("tle1: %f %f %f\n", loc.pos.eci.s.col[0], loc.pos.eci.s.col[1], loc.pos.eci.s.col[2]);
-    printf("tle2: %f %f %f\n", loc2.pos.eci.s.col[0], loc2.pos.eci.s.col[1], loc2.pos.eci.s.col[2]);
+        printf("tle1/2: %f/%f %f/%f %f/%f %f\n", loc.pos.eci.s.col[0], loc2.pos.eci.s.col[0], loc.pos.eci.s.col[1], loc2.pos.eci.s.col[1], loc.pos.eci.s.col[2], loc2.pos.eci.s.col[2], norm_rv(rv_sub(loc2.pos.eci.s,loc.pos.eci.s)));
+    }
 
 }
