926fc8f9a2dc454189d626daa9e1ba0fe1b73430
Eric Pilger <ejpilger@gmail.com>
2021-03-26 11:58:11 -1000

    Check for new Error() class

--- programs/tests/errortest.cpp
+++ programs/tests/errortest.cpp
@@ -6,7 +6,7 @@ int main()
 {
     Error errorlog;
 
-    errorlog.Set(Error::LogType::LOG_FILE_FFLUSH);
+    errorlog.Set(Error::LogType::LOG_STDOUT_FFLUSH);
     errorlog.Printf("Hi there");
-    errorlog.Printf("The Time Is %s\n", utc2iso8601(currentmjd()));
+    errorlog.Printf("The Time Is %s\n", utc2iso8601(currentmjd()).c_str());
 }
