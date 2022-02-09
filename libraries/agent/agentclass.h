4703d407b6b36cc3109624524b73339006984cbb
Eric Pilger <ejpilger@gmail.com>
2022-02-09 13:05:08 -1000

    Disable P2P, enable Loopback only if it is alone

--- libraries/agent/agentclass.h
+++ libraries/agent/agentclass.h
@@ -401,6 +401,8 @@ namespace Cosmos
             string agentName;
             vector<beatstruc> slist;
 
+            int32_t process_request(string &bufferin, string &bufferout);
+
         protected:
         private:
 
@@ -449,7 +451,6 @@ namespace Cosmos
 
             void heartbeat_loop();
             void request_loop() noexcept;
-            int32_t process_request(string &bufferin, string &bufferout);
             void message_loop();
 
             char* parse_request(char *input);
