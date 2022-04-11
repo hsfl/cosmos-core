1276c3bc273aafad2e82a5bc048203c9c02439f5
ginoscott <ginoscottdev@gmail.com>
2022-04-11 08:43:33 -1000

    basic working czml in propagator_web

--- programs/general/propagator_web.cpp
+++ programs/general/propagator_web.cpp
@@ -12,20 +12,25 @@ using namespace Convert;
 
 static Physics::Simulator *sim;
 static double startutc = 0;
+static double endutc;
 static vector<Convert::locstruc> initiallocs;
 static vector<string> nodes;
 // Simulation time step
 static double simdt = 60.;
 // Number of times to increment simulation state (i.e., total simulated time = simdt*runcount)
 static double runcount = 45.;
+static map<string, string> czmls;
 
-string to_czml(const Cosmos::Physics::Simulator::StateList::iterator sit);
+int32_t czml_head();
+int32_t czml_body();
+int32_t czml_foot();
 
 int main(int argc, char *argv[])
 {
     int32_t iretn;
 
     double now = currentmjd();
+    endutc = now + 45./1440.;
 
     string estring;
     json11::Json jargs = json11::Json::parse(argv[1], estring);
@@ -112,21 +117,15 @@ int main(int argc, char *argv[])
         }
     }
 
-    string header;
-    header += "Name\tUTC\t";
-
-    header += "pos_x\tpos_y\tpos_z\t";
-
-    header += "att_w\tatt_x\tatt_y\tatt_z\t";
-
     double mjd = currentmjd();
     string path = data_name("", mjd, "txt", "orbit");
     FILE *ofp = fopen(path.c_str(), "w");
-    fprintf(ofp, "%s\n", header.c_str());
 
     path = data_name("", mjd, "txt", "event");
     FILE *efp = fopen(path.c_str(), "w");
-    fprintf(efp, "%s\n", header.c_str());
+
+    // Add initial header
+    czml_head();
 
     double elapsed = 0;
     while (elapsed < runcount)
@@ -134,54 +133,117 @@ int main(int argc, char *argv[])
         // Step forward in simulation
         sim->Propagate();
 
-        // Populate result
-        string output;
-
-        for (auto sit = sim->cnodes.begin(); sit != sim->cnodes.end(); ++sit)
-        {
-            output += to_czml(sit);
-
-            output += "\n";
-        }
-        fprintf(ofp, "%s\n", output.c_str());
+        // Construct body of czml
+        czml_body();
 
         ++elapsed;
+    }
+
+    // Add footer
+    czml_foot();
+
+    // Output to file
+    for (auto cit = czmls.begin(); cit != czmls.end(); ++cit)
+    {
+        fprintf(ofp, "%s\n", cit->second.c_str());
     }
 
     fclose(ofp);
     fclose(efp);
 }
 
+////////////////////////////////
+// UTILITY FUNCTIONS
+////////////////////////////////
+int32_t czml_head()
+{
+    for (auto sit = sim->cnodes.begin(); sit != sim->cnodes.end(); ++sit)
+    {
+        string& czml = czmls[sit->first];
+        //double utc = sit->second->currentinfo.node.loc.pos.eci.utc;
+        string epoch = utc2iso8601(startutc);
+        string interval = epoch + "/" + utc2iso8601(endutc);
+        czml +=
+        "["
+            "{"
+                "id: \"document\","
+                "name: \"Cesium Orbit Display for Cosmos Web\","
+                "version: \"1.0\","
+            "},"
+            "{"
+                "id: \"" + sit->first + "\","
+                "availability: \"" + interval + "\","
+                "position: {"
+                    "epoch: \"" + epoch + "\","
+                    "referenceFrame: \"INERTIAL\","
+                    "cartesian: [";
+    }
+
+    return 0;
+}
+
 // Create a CZML-format JSON string out of provided values
 // To be consumed by Cosmos Web's Cesium panel
-string to_czml(const Cosmos::Physics::Simulator::StateList::iterator sit)
+int32_t czml_body()
 {
-    string czml;
-    double utc;
-    double px, py, pz;
-    double qw, qx, qy, qz;
-    utc = sit->second->currentinfo.node.loc.pos.eci.utc;
-    px = sit->second->currentinfo.node.loc.pos.eci.s.col[0];
-    py = sit->second->currentinfo.node.loc.pos.eci.s.col[1];
-    pz = sit->second->currentinfo.node.loc.pos.eci.s.col[2];
-    qw = sit->second->currentinfo.node.loc.att.icrf.s.w;
-    qx = sit->second->currentinfo.node.loc.att.icrf.s.d.x;
-    qy = sit->second->currentinfo.node.loc.att.icrf.s.d.y;
-    qz = sit->second->currentinfo.node.loc.att.icrf.s.d.z;
+    string output;
+    for (auto sit = sim->cnodes.begin(); sit != sim->cnodes.end(); ++sit)
+    {
+        double utc;
+        double px, py, pz;
+        // double qw, qx, qy, qz;
+        utc = sit->second->currentinfo.node.loc.pos.eci.utc;
+        px = sit->second->currentinfo.node.loc.pos.eci.s.col[0];
+        py = sit->second->currentinfo.node.loc.pos.eci.s.col[1];
+        pz = sit->second->currentinfo.node.loc.pos.eci.s.col[2];
+        // qw = sit->second->currentinfo.node.loc.att.icrf.s.w;
+        // qx = sit->second->currentinfo.node.loc.att.icrf.s.d.x;
+        // qy = sit->second->currentinfo.node.loc.att.icrf.s.d.y;
+        // qz = sit->second->currentinfo.node.loc.att.icrf.s.d.z;
 
-    czml += sit->second->currentinfo.node.name + "\t";
-    czml += to_mjd(utc) + "\t";
+        // Get appropriate czml string of node_name
+        string& czml = czmls[sit->first];
+        // Time offset from specified epoch (in seconds)
+        czml += to_floatany(86400.*(utc-startutc)) + ",";
 
-    // ECI
-    czml += to_floatany(px) + "\t";
-    czml += to_floatany(py) + "\t";
-    czml += to_floatany(pz) + "\t";
+        // ECI
+        czml += to_floatany(px) + ",";
+        czml += to_floatany(py) + ",";
+        czml += to_floatany(pz) + ",\n";
 
-    // attitudes
-    czml += to_floatany(qw) + "\t";
-    czml += to_floatany(qx) + "\t";
-    czml += to_floatany(qy) + "\t";
-    czml += to_floatany(qz) + "\t";
+        // attitudes
+        /*czml += to_floatany(qw) + "\t";
+        czml += to_floatany(qx) + "\t";
+        czml += to_floatany(qy) + "\t";
+        czml += to_floatany(qz) + "\t";*/
+    }
 
-    return czml;
+    return 0;
 }
+
+int32_t czml_foot()
+{
+    for (auto sit = sim->cnodes.begin(); sit != sim->cnodes.end(); ++sit)
+    {
+        string& czml = czmls[sit->first];
+        czml +=     "],"    
+                "},"
+                "point: {"
+                "color: {"
+                    "rgba: [255, 255, 255, 128],"
+                "},"
+                "outlineColor: {"
+                    "rgba: [255, 0, 0, 128],"
+                "},"
+                "outlineWidth: 3,"
+                "pixelSize: 15,"
+                "},"
+            "},"
+        "]";
+    }
+
+    return 0;
+}
+
+// TODO:
+// need to fix json array to support multiple sats
