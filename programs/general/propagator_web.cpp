04a6ca566ba2782e1062511949a076efd0e63bcc
Miguel Nunes <spacemig@gmail.com>
2022-05-18 00:50:27 +0100

    Update propagator_web.cpp

--- programs/general/propagator_web.cpp
+++ programs/general/propagator_web.cpp
@@ -4,6 +4,7 @@
 #include "support/packetcomm.h"
 #include <iomanip>
 #include <sstream>
+#include <cmath>
 
 using namespace Convert;
 
@@ -46,6 +47,10 @@ int32_t czml_foot(prop_unit& prop, string& output);
 
 // Other functions
 int32_t calculate_distances(prop_unit& prop);
+double degreesToRadians(double degrees);
+double integral(double(*f)(double x), double a, double b, int n);
+double IntBck(double x);
+double vlc_snr(double distance);
 
 int main(int argc, char *argv[])
 {
@@ -397,14 +402,202 @@ int32_t calculate_distances(prop_unit& prop)
             dy = py2 - py1;
             dz = pz2 - pz1;
             d = sqrt(dx*dx + dy*dy + dz*dz);
+            
+            // vlc model using calculated distance as input
+            double snr = vlc_snr(d);
 
             // Worry about what to do with this later
             cout << std::fixed;
             cout << std::setprecision(8) << utc << " ";
-            cout << std::setprecision(precision) << node_1 << ":" << node_2 << " " << d << endl;
+            cout << std::setprecision(precision) << node_1 << ":" << node_2 << " " << d << snr << endl;
         }
     }
 
     return 0;
 }
+
+// -------------------------------------
+// code for new vlc system
+double degreesToRadians(double degrees)
+{
+    return degrees * M_PI / 180;
+}
+
+// compute integral for function pointer
+double integral(double(*f)(double x), double a, double b, int n) {
+    double step = (b - a) / n;  // width of each small rectangle
+    double area = 0.0;  // signed area
+    for (int i = 0; i < n; i ++) {
+        area += f(a + (i + 0.5) * step) * step; // sum up each small rectangle
+    }
+    return area;
+}
+
+double IntBck(double x){
+    return pow(x,3)/(exp(x)-1);
+}
+
+double vlc_snr(double distance) {
+    
+    // from Table IV
+    //Semi-angle at Half Power, Φ1/2
+    double semi_angle_at_half_power = 30; // deg
+    double phi_ = degreesToRadians(semi_angle_at_half_power); // convert to radians
+
+    //LED Peak Wavelength, λpeak
+    double led_peak_wavelength = 656.2808; //nm
+    led_peak_wavelength = led_peak_wavelength*1e-9; // convert to meters
+
+    // Concentrator FoV Semi-angle, ψc
+    double concentrator_fov_semi_angle = 35; //deg
+    double psi_c = degreesToRadians(concentrator_fov_semi_angle); // convert to radians
+
+    // Filter Transmission Coefficient (or gain), To or Ts
+    double Ts = 1;
+
+    // Irradiance Angle, ψ
+    double irradiance_angle = 15; //deg //TODO: not being used
+    // Detector Responsivity, gamma
+    double detector_responsivity = 0.51;
+    //double gamma = detector_responsivity;
+    // Refractive Index of Lens, n
+    double n = 1.5;
+
+    //Radius of Concentrator, R
+    double R = 2; //cm
+    // Detector Active Area, Apd
+    double Apd = 7.84; //cm^2
+    Apd = Apd*1e-4; // convert to m^2
+    // Desired Electrical Bandwidth, B
+    double B = 0.5; //MHz
+    B = B*1e6; // convert to Hz
+    // Optical Filter Bandwidth, ∆λ
+    double d_lambda = 0.4020; //nm
+    d_lambda = 0.4020*1e-9; // convert to meters
+    // Optical Filter Lower Limit, lambda_1
+    double lambda_1 = 656.0798; //nm
+    lambda_1 = lambda_1*1e-9; // convert to meters
+    // Optical Filter Upper Limit, lambda_2
+    double lambda_2 = 656.4818; //nm
+    lambda_2 = lambda_2*1e-9; // convert to meters
+    // Open Loop Voltage Gain,
+    double G = 10;
+    // FET Transconductance, gm
+    double gm = 30; // ms
+    gm = 30*1e-3; // convert to sec
+    // FET Channel Noise Factor, gamma
+    double gamma_large = 0.82; // or 1.5
+    // Capacitance of Photodetector, etta
+    double etta = 38; //pF /cm^2
+    etta = etta*1e-8; // convert to Farad / m^2
+    // Noise Bandwidth Factor for White Noise, I2
+    double I2 = 0.562;
+    //Noise Bandwidth Factor for f^2 noise, I3
+    double I3 = 0.0868;
+    // Boltzmann Constant, k
+    double k = 1.380649e-23; //J/K
+    // Absolute Temperature, TA
+    double Ta = 300; //K
+
+    // c is the speed of light,
+    double c = 299792458.0; // m/sec
+    // hp is Planck's constant
+    double hp = 6.62607015e-34;
+    // T is average temperature of the Sun's surface.
+    double Tsun = 5778; // K
+
+
+    // -------------- distance from cosmos simulation
+    //double distance = 10.79707118;
+    // -------------- time from cosmos simulation
+
+
+    // The angle of incidence with respect to the receiver axis is ψ,
+    // test for now with
+    double psi = 0;
+
+    // transmitted optical power
+    double Pt = 5.6; //W
+
+    // Incidence Angle, φ / transmitter perpendicular axis
+    double incidence_angle = 0; //deg, was 30
+    double phi = degreesToRadians(incidence_angle); // convert to radians
+
+    double g_psi = 0;
+    // concentrator gain, g(ψ)
+    if (0 <= degreesToRadians(psi) and  degreesToRadians(psi) <= psi_c) {
+        g_psi = (pow(n,2))/(pow(sin(psi_c),2));
+    } else {
+        g_psi = 0;
+    }
+
+
+    
+    // Signal Model
+
+    // received optical power, Pr
+    // the channel DC gain, H0
+    // lambertian emission, m
+    double m = -log(2.)/log(cos(phi_));
+
+    double Hl1 = (Apd*(m + 1))/(2.*M_PI*pow(distance,2));
+    double Hl2 = (cos(psi))*(pow(cos(phi),m));
+
+
+    //
+    double H0;
+   
+    if (0 <= degreesToRadians(psi) and degreesToRadians(psi) <= psi_c) {
+        H0 = g_psi*Hl1*Hl2*Ts;         // Channel  Gain;
+        //std::cout << "test" << std::endl;
+    } else {
+        H0 = 0;
+    }
+    // std::cout << H0 << std::endl;
+
+    double Pr = H0 * Pt;
+    double S = detector_responsivity * Pr;
+    S = S*S;
+
+
+
+    // q is the electronic charge,
+    double q = 1.60217662e-19; //coulombs
+    double Rse = 150e9;                   // Distance between Sun and Earth
+    double SAsun = 4*M_PI*pow(Rse,2);  // Surface area of Sun
+    double day = 15;                     // Day of year
+
+    double xmin = (hp * c)/(lambda_1*k*Tsun);
+    double xmax = (hp * c)/(lambda_2*k*Tsun);
+
+    // computation of the integral (eq 9)
+    //IntBck = lambda y: np.power(y,3)/(np.exp(y)-1)     # Computing background noise produced by Sun
+    double Bck_NI = integral(IntBck,xmin,xmax,10);
+
+    double PwrArea = ((2*M_PI*pow(k,4)*pow(Tsun,4)*Bck_NI))/(pow(hp,3)*pow(c,2));
+    double So = PwrArea/SAsun;
+
+    double ESD = 1.00011 + 0.034211*cos(B) + 0.00128*sin(B) + 0.000719*cos(2*B) + 0.000077*sin(2*B);
+    double Sod = So*ESD;
+    double E_det = Sod;
+
+    // background noise power detected by the optical receiver
+    double Pbg = E_det*Ts*Apd*pow(n,2);
+
+    //shot noise
+    double sigma_s_2 = 2*q*detector_responsivity*(Pr + I2*Pbg)*B; // TODO: confirm 0.5 factor, fix Pbg
+
+    double SoN = S/sigma_s_2; // Recieved power over shot noise
+    double Snr = 10 * log10(SoN);
+
+    // std::cout << Snr << std::endl;
+
+    return Snr;
+}
+
+
+
+
+
+
 
