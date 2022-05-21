f8c7004d6df0b21612adfd2c0f0711c4350442ad
Eric Pilger <ejpilger@gmail.com>
2022-05-20 22:13:16 -1000

    Replacing queues with Channels

---
+++ libraries/support/channellib.cpp
@@ -0,0 +1,6 @@
+#include "channellib.h"
+
+channellib::channellib()
+{
+
+}
---
+++ libraries/support/channellib.h
@@ -0,0 +1,11 @@
+#ifndef CHANNELLIB_H
+#define CHANNELLIB_H
+
+
+class channellib
+{
+public:
+    channellib();
+};
+
+#endif // CHANNELLIB_H
