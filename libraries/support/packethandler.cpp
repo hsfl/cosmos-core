a4b24dc6a824ba690e7b5d00c7cbb0bd2a253b67
ginoscott <ginoscottdev@gmail.com>
2022-08-30 13:38:08 -1000

    remove ResponseHandler

--- libraries/support/packethandler.cpp
+++ libraries/support/packethandler.cpp
@@ -61,50 +61,6 @@ namespace Cosmos {
             return 0;
         }
 
-        int32_t PacketHandler::register_response(RespCallback f)
-        {
-            if (get_next_packet_id()) {
-                response_packets[packet_idx].set_callback(f);
-                return static_cast<int32_t>(packet_idx);
-            }
-            // TODO: return proper error code
-            cout << "Error in PacketHandler::register_response(), no available packet_id" << endl;
-            return -1;
-        }
-
-        /** Insert data of PacketComm packet of type Response into appropriate
-         *  response_packets slot. Assumes packet is valid, and a proper response
-         *  packet type.
-         */
-        int32_t PacketHandler::receive_response_packet(const PacketComm &packet)
-        {
-            uint32_t response_id = uint32from((uint8_t*)&packet.data[0], ByteOrder::LITTLEENDIAN);
-            if(response_packets[response_id].is_available()) {
-                cout << "Error in PacketHandler, no response registered for response_id: " << response_id << endl;
-                // TODO: return proper error code
-                return -1;
-            }
-            response_packets[response_id].insert(packet.data);
-            response_packets[response_id].attempt_resolution(60.);
-
-            return 0;
-        }
-
-        bool PacketHandler::get_next_packet_id()
-        {
-            for (int i = 0; i < 256; ++i) {
-                packet_idx += i;
-                if (packet_idx >= 256) {
-                    packet_idx %= 256;
-                }
-                if (response_packets[packet_idx].is_available()) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
         /**
          * Create a response type packet to be addressed to the
          * packet id contained within the addressee packet
@@ -140,79 +96,6 @@ namespace Cosmos {
                 packets.push_back(packet);
             }
             return packets;
-        }
-
-        //////////////////////////////////////////////////////////////
-        //                     Response Handler                     //
-        //////////////////////////////////////////////////////////////
-
-        ResponseHandler::ResponseHandler()
-        {
-            callback = nullptr;
-        }
-
-        bool ResponseHandler::is_available()
-        {
-            return callback == nullptr;
-        }
-
-        void ResponseHandler::set_callback(const RespCallback f) {
-            callback = f;
-        }
-
-        int32_t ResponseHandler::insert(const vector<uint8_t> &bytes)
-        {
-            uint8_t chunk_id = bytes[3];
-            uint8_t total_chunks = bytes[4];
-            // Resize vector to expected response size
-            if (data.size() != total_chunks) {
-                data.resize(total_chunks);
-            }
-            // Check if packet index has already been receieved
-            if (data[chunk_id].size() > 0) {
-                return 0;
-            }
-            // starting index of response data
-            int data_start = 5;
-            data[chunk_id] = vector<uint8_t>(bytes.begin() + data_start, bytes.end());
-            last_receieved_time = currentmjd();
-
-            return 0;
-        }
-
-        int32_t ResponseHandler::attempt_resolution(const double lapse) {
-            double lapse_mjd = lapse / 86400;
-            bool all_packets_received = true;
-            // Check if all expected packets have been receieved
-            for (auto p : data) {
-                if (p.size() == 0) {
-                    all_packets_received = false;
-                    break;
-                }
-            }
-            // Only continue if lapse time has passed or all expected packets received
-            if (currentmjd() < last_receieved_time + lapse_mjd && !all_packets_received) {
-                return 0;
-            }
-
-            // Proceed with response resolution since conditions are cleared
-            vector<uint8_t> full_response;
-            for (auto p : data) {
-                full_response.insert(full_response.end(), p.begin(), p.end());
-            }
-
-            // Resolve resposne
-            callback(full_response);
-            // Reset to initial, make this available for reuse
-            reset();
-
-            return 0;
-        }
-
-        void ResponseHandler::reset() {
-            callback = nullptr;
-            last_receieved_time = 0;
-            vector<vector<uint8_t>>().swap(data);
         }
 
         int32_t PacketHandler::process(PacketComm& packet)
--- libraries/support/packethandler.h
+++ libraries/support/packethandler.h
@@ -12,33 +12,6 @@
 
 namespace Cosmos {
     namespace Support {
-        class ResponseHandler
-        {
-        public:
-            ResponseHandler();
-            /// Insert indexed response packet's data into data vector
-            int32_t insert(const vector<uint8_t> &bytes);
-            /// Attempt callback execution on currently received data.
-            /// Will succeed if last indexed packet has been receieved
-            /// or if a specified amount of time has passed (in seconds).
-            int32_t attempt_resolution(const double lapse);
-            /// Check if this response packet is available for use
-            bool is_available();
-            typedef int32_t (*RespCallback)(const vector<uint8_t>&);
-            /// Setter
-            void set_callback(RespCallback f);
-        private:
-            // Holds indexed packet response data
-            vector<vector<uint8_t>> data;
-            // Execute this callback function when data is all receieved
-            RespCallback callback;
-            /// Timestamp of last receieved packet, use when cleaning up lost responses
-            double last_receieved_time;
-
-            // Make this response handler available for reuse after resolution
-            void reset();
-        };
-
         class PacketHandler
         {
         public:
@@ -119,8 +92,6 @@ namespace Cosmos {
 
         private:
             Agent* agent;
-            /// Register response callbacks in this array
-            ResponseHandler response_packets[256];
             /// Current packet id index, to index response_packets
             uint16_t packet_idx;
 
