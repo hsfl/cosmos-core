7d04cbaa6438f0cc60f463f9061d848b314913dd
ginoscott <ginoscottdev@gmail.com>
2023-01-17 10:11:41 -1000

    Unwrap explicit minimal header check

--- libraries/support/packetcomm.h
+++ libraries/support/packetcomm.h
@@ -743,6 +743,7 @@ namespace Cosmos {
             void CalcCRC();
             bool CheckCRC();
             int32_t Unwrap(bool checkcrc=true);
+            int32_t Unwrap(bool checkcrc, bool minimal_header);
             int32_t RawUnPacketize(bool invert=false, bool checkcrc=true);
             bool ASMUnPacketize(bool checkcrc=true);
             bool SLIPUnPacketize(bool checkcrc=true);
