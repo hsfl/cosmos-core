03fcfd794c0bc478d22debf6d78921587230e5b8
Ethan Chee <ethancheez@gmail.com>
2023-01-24 11:56:30 -1000

    Remove unnecessary segfault check for uint8_t

--- libraries/agent/agentclass.cpp
+++ libraries/agent/agentclass.cpp
@@ -4171,13 +4171,7 @@ acquired.
 
         int32_t Agent::channel_set_comm_priority(uint8_t number)
         {
-            if (number < 0 || number > 255)
-            {
-                return GENERAL_ERROR_OUTOFRANGE;
-            }
-            
             channels.comm_id = number;
-
             return 0;
         }
 
