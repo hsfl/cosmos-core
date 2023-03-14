69c83d71169895359a4b4d5d607a9088825b9ae5
Ethan Chee <ethancheez@gmail.com>
2023-03-13 18:57:54 -1000

    use decisec to authenticate encrypted packet

--- libraries/support/crypto.cpp
+++ libraries/support/crypto.cpp
@@ -4,6 +4,8 @@ namespace Cosmos
 {
     namespace Support
     {
+        std::atomic<uint32_t> Crypto::prev_sequence{0};
+
         Crypto::Crypto()
         {
             ctx = EVP_CIPHER_CTX_new();
@@ -37,45 +39,60 @@ namespace Cosmos
             return iv;
         }
 
-        vector<uint8_t> Crypto::encrypt(vector<uint8_t> plaintext_str)
+        int32_t Crypto::encrypt(vector<uint8_t> plaintext_str, vector<uint8_t> &ciphertext_str)
         {
             int ciphertext_len;
-            vector<uint8_t> buf(plaintext_str.size());
-            vector<uint8_t> ciphertext_str;
+            vector<uint8_t> buf(plaintext_str.size() + 4);
+            vector<uint8_t> plaintext;
+
+            plaintext.resize(4);
+            uint32to(decisec(), plaintext.data());
+            plaintext.insert(plaintext.end(), plaintext_str.begin(), plaintext_str.end());
 
             EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv.size(), NULL);
             EVP_EncryptInit_ex(ctx, NULL, NULL, (unsigned char *)key.c_str(), iv.data());
-            EVP_EncryptUpdate(ctx, buf.data(), &ciphertext_len, plaintext_str.data(), plaintext_str.size());
+            EVP_EncryptUpdate(ctx, buf.data(), &ciphertext_len, plaintext.data(), plaintext.size());
 
+            ciphertext_str.clear();
             for (int i = 0; i < ciphertext_len; i++)
             {
                 ciphertext_str.push_back(buf[i]);
             }
-            return ciphertext_str;
+
+            ciphertext_str.insert(ciphertext_str.end(), iv.begin(), iv.end());
+
+            return 0;
         }
 
-        vector<uint8_t> Crypto::decrypt(vector<uint8_t> ciphertext_str)
+        int32_t Crypto::decrypt(vector<uint8_t> ciphertext_str, vector<uint8_t> &plaintext_str)
         {
             int plaintext_len;
-            vector<uint8_t> buf(ciphertext_str.size());
-            vector<uint8_t> plaintext_str;
+            vector<uint8_t> buf(ciphertext_str.size() + 4);
+            vector<uint8_t> plaintext;
 
-            for (size_t i = 0; i < ciphertext_str.size() - iv.size(); i++)
-            {
-                plaintext_str.push_back(ciphertext_str[i]);
-            }
+            plaintext.insert(plaintext.begin(), ciphertext_str.begin(), ciphertext_str.end() - iv.size());
 
             EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv.size(), NULL);
             EVP_DecryptInit_ex(ctx, NULL, NULL, (unsigned char *)key.c_str(), iv.data());
-            EVP_DecryptUpdate(ctx, buf.data(), &plaintext_len, plaintext_str.data(), plaintext_str.size());
+            EVP_DecryptUpdate(ctx, buf.data(), &plaintext_len, plaintext.data(), plaintext.size());
+
+            vector<uint8_t> sequence(4);
+            memcpy(sequence.data(), &buf[0], 4);
+
+            uint32_t curr_sequence = uint32from(sequence.data());
+            if (curr_sequence <= prev_sequence)
+            {
+                return COSMOS_GENERAL_ERROR_BAD_ACK;
+            }
 
             plaintext_str.clear();
-            for (int i = 0; i < plaintext_len; i++)
+            for (int i = 4; i < plaintext_len; i++)
             {
                 plaintext_str.push_back(buf[i]);
             }
 
-            return plaintext_str;
+            prev_sequence = curr_sequence;
+            return 0;
         }
     }
 
--- libraries/support/crypto.h
+++ libraries/support/crypto.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <atomic>
 #include "support/configCosmos.h"
+#include "support/timelib.h"
+#include "math/bytelib.h"
 #include <openssl/aes.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
@@ -15,20 +17,20 @@ namespace Cosmos
         class Crypto
         {
         public:
+            static std::atomic<uint32_t> prev_sequence;
+
             Crypto();
             ~Crypto();
             int32_t setKey(string key);
             int32_t setIV(vector<uint8_t> iv);
             vector<uint8_t> getRandIV(uint8_t size);
-            vector<uint8_t> encrypt(vector<uint8_t> plaintext_str);
-            vector<uint8_t> decrypt(vector<uint8_t> ciphertext_str);
+            int32_t encrypt(vector<uint8_t> plaintext_str, vector<uint8_t> &ciphertext_str);
+            int32_t decrypt(vector<uint8_t> ciphertext_str, vector<uint8_t> &plaintext_str);
 
         private:
             EVP_CIPHER_CTX *ctx;
             string key = "secretkey";
             vector<uint8_t> iv;
-
-            static std::atomic<uint16_t> sequence; // TODO
         };
     }
 
