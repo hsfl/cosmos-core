45b07f838df6e904f3d33bcdc277da784dec7937
Eric Pilger <ejpilger@gmail.com>
2024-06-20 08:36:15 -1000

    Directory name change

--- cmake/toolchain_iobc_linux.cmake
+++ cmake/toolchain_iobc_linux.cmake
@@ -1,10 +1,10 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CROSS_TYPE iobc)
 
-SET(CMAKE_C_COMPILER   /home/shared/software/kubos/iobc_toolchain/bin/arm-linux-gcc)
-SET(CMAKE_CXX_COMPILER /home/shared/software/kubos/iobc_toolchain/bin/arm-linux-g++)
+SET(CMAKE_C_COMPILER   /home1/shared/software/kubos/iobc_toolchain/bin/arm-linux-gcc)
+SET(CMAKE_CXX_COMPILER /home1/shared/software/kubos/iobc_toolchain/bin/arm-linux-g++)
 
-SET(CMAKE_FIND_ROOT_PATH  /home/shared/software/kubos/iobc_toolchain/arm-buildroot-linux-gnueabi)
+SET(CMAKE_FIND_ROOT_PATH  /home1/shared/software/kubos/iobc_toolchain/arm-buildroot-linux-gnueabi)
 
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
