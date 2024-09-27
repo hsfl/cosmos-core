e34e801209976f829ac3daab207ec33593d6113e
JRL <jrlabs.org@gmail.com>
2024-09-27 13:19:07 -0500

    add more tests for ParseTargetString and ParseTargetJson

--- programs/tests/targetstruc_tests.cpp
+++ programs/tests/targetstruc_tests.cpp
@@ -48,29 +48,59 @@ int main(int argc, char *argv[])
 	S.AddTarget("Fake", 1.23, 2.34, 4.001, 13, NODE_TYPE_TARGET);
 	cout<<"cosmosstruc targets = "<<S.cnodes[0]->currentinfo.target.size()<<endl;
 
-	// Try to parse the default Target File
-	S.ParseTargetFile(string());
+	// Try to parse the default Target File as a single line
+	//S.ParseTargetFile("../python/json/cosmos_targets2.json");
 	cout<<"cosmosstruc targets = "<<S.cnodes[0]->currentinfo.target.size()<<endl;
+
+	// Try to parse the default Target File
+	std::ifstream infile2("../python/json/cosmos_targets.json");
+
+	// Check if the file was opened successfully
+	if (!infile2.is_open()) {
+		std::cerr << "Error: Could not open the file!" << std::endl;
+		return 1;
+	}
+	
+	// Read the file into a string (json_str)
+	string targets_json2((std::istreambuf_iterator<char>(infile2)),std::istreambuf_iterator<char>());
+	
+	// Close the file
+	infile2.close();
+
+	cout<<"string = "<<targets_json2<<endl;
+	string estring;
+	json11::Json jargs = json11::Json::parse(targets_json2, estring);
+
+	// Check for any parsing errors
+	if (!estring.empty()) {
+		std::cerr << "Error parsing JSON: " << estring << std::endl;
+		return -1;
+	}
+	S.ParseTargetJson(jargs);
+
+
+
+
 
 	// Add targets from JSON string
 
 	// File path to the JSON file
-    std::ifstream infile("../python/json/cosmos_targets2.json");
-    
-    // Check if the file was opened successfully
-    if (!infile.is_open()) {
-        std::cerr << "Error: Could not open the file!" << std::endl;
-        return 1;
-    }
-    
-    // Read the file into a string (json_str)
+	std::ifstream infile("../python/json/cosmos_targets2.json");
+	
+	// Check if the file was opened successfully
+	if (!infile.is_open()) {
+		std::cerr << "Error: Could not open the file!" << std::endl;
+		return 1;
+	}
+	
+	// Read the file into a string (json_str)
 	string targets_json((std::istreambuf_iterator<char>(infile)),std::istreambuf_iterator<char>());
-    
-    // Close the file
-    infile.close();
+	
+	// Close the file
+	infile.close();
 
 	// Load Json string into targets
-	S.ParseTargetString(targets_json);
+	//S.ParseTargetString(targets_json);
 	cout<<"cosmosstruc targets = "<<S.cnodes[0]->currentinfo.target.size()<<endl;
 	return 0;
 
